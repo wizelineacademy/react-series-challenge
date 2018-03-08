444c5b071b0475eb6d75d692825c96659af3b2cf
Alberto Sánchez <krilbert@gmail.com>
2018-03-08 11:44:52 -0600

    Basic page styling

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { PageWrapper } from './styled-components';
 import { Home, Favorites, Details, Search, NotFound } from './views';
+import './views/css/reset.css';
+import './views/css/style.css';
 
 export default () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/favorites" component={Favorites} />
-      <Route exact path="/gifs/:id" component={Details} />
-      <Route exact path="/search/:text" component={Search} />
-      <Route component={NotFound} />
-    </Switch>
-  </Router>
+  <PageWrapper>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/favorites" component={Favorites} />
+        <Route exact path="/gifs/:id" component={Details} />
+        <Route exact path="/search/:text" component={Search} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  </PageWrapper>
 );
---
+++ src/styled-components/page-wrapper.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export default styled.section`
+  color: #b2b2b2;
+  width: 1040px;
+  margin: 0px auto;
+`;
