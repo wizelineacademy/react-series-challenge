6e47faa24614385259510ec530c6821aa13ae4ed
betoleal <alberto.leal@wizeline.com>
2018-03-09 17:31:01 -0600

    Implement Sagas

--- src/index.js
+++ src/index.js
@@ -8,15 +8,18 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga'
 
 import reducers from './reducers';
+import rootSaga from './sagas'
 import registerServiceWorker from './registerServiceWorker';
 
 import Home from './component/Home'
 
 const sagaMiddleware = createSagaMiddleware()
 const createStoreWithMiddleware = composeWithDevTools(applyMiddleware(sagaMiddleware))(createStore)
+const store = createStoreWithMiddleware(reducers)
+sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={createBrowserHistory()}>
       <Route path="/" component={Home}/>
     </Router>
