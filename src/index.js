0f201f88f51ecfc4633b06b2b8717c65d305de10
betoleal <alberto.leal@wizeline.com>
2018-03-09 15:01:37 -0600

    Add home component

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ import createBrowserHistory from 'history/createBrowserHistory'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga'
 
-import App from './App';
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
+
+import Home from './component/Home'
 
 const sagaMiddleware = createSagaMiddleware()
 const createStoreWithMiddleware = composeWithDevTools(applyMiddleware(sagaMiddleware))(createStore)
@@ -17,7 +18,7 @@ const createStoreWithMiddleware = composeWithDevTools(applyMiddleware(sagaMiddle
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={createBrowserHistory()}>
-      <Route path="/" component={App}/>
+      <Route path="/" component={Home}/>
     </Router>
   </Provider>
   , document.getElementById('root'));
