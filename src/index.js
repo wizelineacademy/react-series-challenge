800f9e28aa95f01c442607c626162280e45da93f
betoleal <alberto.leal@wizeline.com>
2018-03-09 10:52:59 -0600

    Add app scaffold

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, Route} from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga'
+
 import App from './App';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware()
+const createStoreWithMiddleware = composeWithDevTools(applyMiddleware(sagaMiddleware))(createStore)
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={createBrowserHistory()}>
+      <Route path="/" component={App}/>
+    </Router>
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
