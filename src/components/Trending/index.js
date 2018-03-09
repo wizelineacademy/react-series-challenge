4896f31e94d012b452b280ec9389787260c6e814
bgilm <benjamingil94@gmail.com>
2018-03-09 12:35:28 -0600

    Create dummy Trending component

---
+++ src/components/Trending/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Trending extends Component {
+  render() {
+    return(
+      <div>
+        Trending
+      </div>
+    )
+  }
+}
+
+export default Trending
