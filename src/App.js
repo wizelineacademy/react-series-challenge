540b80e94396497ac700b469b181e67b4ba6e2b5
bgilm <benjamingil94@gmail.com>
2018-03-09 13:11:01 -0600

    Render container from route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './store'
-import logo from './logo.svg';
 import './App.css';
+import {
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+import TrendingContainer from './containers/Trending'
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Switch>
+            <Route path="/" component={TrendingContainer}  />
+            <Redirect to={{
+              pathname:"/",
+              state: { from: 'PAGE NOT FOUND'},
+            }} />
+          </Switch>
         </div>
       </Provider>
     );
