bd1aecc7274361b2e9b7c9a9ef9cb80811bb85e3
bgilm <benjamingil94@gmail.com>
2018-03-09 15:41:41 -0600

    Use 50 as a default trending limit

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 const API_KEY = 'IQkt47mC1gSCSWSm7UomSm3O8rdcBIt4'
 const BASE_URL = 'https://api.giphy.com/v1/gifs/'
 
-export const getTrending = () => fetch(`${BASE_URL}trending?api_key=${API_KEY}`)
+export const getTrending = () => fetch(`${BASE_URL}trending?api_key=${API_KEY}&limit=50`)
 .then(res => res.json())
