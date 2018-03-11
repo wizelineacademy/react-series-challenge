01cfd8531439e226c5f7d26c3367a7283a9c7fc3
Alberto Sánchez <krilbert@gmail.com>
2018-03-11 14:45:55 -0600

    Adding Fav button to thumbnails

--- src/components/thumbnail.jsx
+++ src/components/thumbnail.jsx
@@ -8,6 +8,45 @@ const ThumbnailWrapper = styled.section`
   position: absolute;
 `;
 
+const IconsDiv = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+
+  :hover a {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+const Icon = styled.a`
+  margin: 2px;
+  opacity: 0;
+  transform: translateY(50%);
+
+  > i {
+    display: block;
+    font-size: 23.52941176px;
+    line-height: 40px;
+    width: 40px;
+    background-color: #ffffff;
+    text-align: center;
+    color: #000000;
+  }
+
+  > i:hover {
+    background-color: #4da3e2;
+    color: #ffffff;
+    cursor: pointer;
+  }
+`;
+
 export default class Thumbnail extends Component {
   static propTypes = {
     id: PropTypes.string.isRequired,
@@ -35,6 +74,9 @@ export default class Thumbnail extends Component {
     return (
       <ThumbnailWrapper>
         <Link to={`/gifs/${id}`}><img src={url} alt={title} width={width} height={height} /></Link>
+        <IconsDiv>
+          <Icon alt="Fav"><i className="far fa-thumbs-up" /></Icon>
+        </IconsDiv>
       </ThumbnailWrapper>
     );
   }
