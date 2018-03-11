cfa653ee4506a53dc197c8d61f8156a47638d2d2
bgilm <benjamingil94@gmail.com>
2018-03-11 15:30:50 -0600

    Add new reducer to combined index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import trendingGifs from './trending'
+import favoriteGifs from './favorites'
 
 export default combineReducers({
-  trendingGifs
+  trendingGifs,
+  favoriteGifs
 });
