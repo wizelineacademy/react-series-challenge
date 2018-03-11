f45067fa4e5126124a17b79434ce98e4122abf4d
betoleal <alberto.leal@wizeline.com>
2018-03-11 10:13:09 -0600

    implement add and remove actions

--- src/reducers/favorites-reducer.js
+++ src/reducers/favorites-reducer.js
@@ -1,5 +1,13 @@
+import {ADDED_FAVORITE, DELETED_FAVORITE} from '../actions/types'
+
 export default function(state={}, action) {
   switch(action.type){
+    case ADDED_FAVORITE:
+      return {...state, [action.payload.id]: action.payload}
+    case DELETED_FAVORITE:
+      const oldState = state
+      delete oldState[action.payload.id]
+      return { ...oldState }
     default:
       return state
   }
