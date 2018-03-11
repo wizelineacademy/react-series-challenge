09fa95f2ae67add3119fe70e71496c1a5deb4c6d
Alberto Sánchez <krilbert@gmail.com>
2018-03-11 14:25:22 -0600

    Shouldn't use Link outside Router

--- src/components/user-card.jsx
+++ src/components/user-card.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { ProfileAvatar } from '../components';
@@ -18,7 +17,7 @@ const UserCardWrapper = styled.div`
 
 const UserCard = ({ data: { username, display_name, profile_url, avatar_url, is_verified } }) => (
   <UserCardWrapper>
-    <Link to={profile_url}><ProfileAvatar src={avatar_url} alt={display_name} /></Link>
+    <a href={profile_url}><ProfileAvatar src={avatar_url} alt={display_name} /></a>
     <div>{ display_name }</div>
     <div>{ `@${username}` } { is_verified ? 'Verified' : '' }</div>
   </UserCardWrapper>
