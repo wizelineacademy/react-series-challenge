e29a796d3ab2f95e0f549196d5c7c0528ca4664b
bgilm <benjamingil94@gmail.com>
2018-03-12 12:48:20 -0600

    Add gif item component

---
+++ src/components/GifItem/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import {
+  GifItemWrapper,
+  StyledButton
+} from '../Trending/Trending.style'
+
+class GifItem extends Component {
+  render() {
+    const { gif, isFavorite } = this.props
+    return(
+      <GifItemWrapper key={gif.id}>
+        <StyledButton
+           onClick={() => this.props.setFavorite(gif)}
+           isFavorite={isFavorite}
+        >
+           LIKE
+         </StyledButton>
+        <img alt={gif.title} src={gif.images.fixed_width_downsampled.url} />
+      </GifItemWrapper>
+    )
+  }
+}
+
+export default GifItem
