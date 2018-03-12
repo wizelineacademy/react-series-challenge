c0e59bd60fcaef10dc38fdc5c76ae04f8d2ece3d
bgilm <benjamingil94@gmail.com>
2018-03-12 13:46:26 -0600

    Persist state for favorites

--- src/store/index.js
+++ src/store/index.js
@@ -3,17 +3,25 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers';
 import rootSaga from '../sagas';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { loadState, saveState } from './localStorage'
 
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [
   sagaMiddleware
 ];
 
-const store = createStore(rootReducer,
+const persistedState = loadState()
+const store = createStore(rootReducer, persistedState,
   composeWithDevTools(
     applyMiddleware(...middleware)
   )
 );
+
+store.subscribe(() => {
+  saveState({
+    favoriteGifs: store.getState().favoriteGifs
+  })
+})
 
 sagaMiddleware.run(rootSaga);
 
