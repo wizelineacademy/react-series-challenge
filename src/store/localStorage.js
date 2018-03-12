2e337e1feb663c8f197a9673933d02de30236d2d
bgilm <benjamingil94@gmail.com>
2018-03-12 17:18:50 -0600

    Remove unecessary log

--- src/store/localStorage.js
+++ src/store/localStorage.js
@@ -11,7 +11,6 @@ export const loadState = () => {
 }
 
 export const saveState = (state) => {
-  console.log(state);
   try {
     const serializedState = JSON.stringify(state)
     localStorage.setItem('state', serializedState)
