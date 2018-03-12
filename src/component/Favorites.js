e9587bfd6457de6f4d168dcf1857b6baa94cb4b1
betoleal <alberto.leal@wizeline.com>
2018-03-12 10:24:28 -0600

    Create Favorites component

---
+++ src/component/Favorites.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import SearchBar from './SearchBar'
+import ListGiphy from './ListGimphy'
+
+import {deleteFavorite} from '../actions'
+
+class Favorites extends Component {
+
+  handleOnclick (giphy) {
+    this.props.deleteFavorite(giphy)
+  }
+
+  listGiphy (giphys) {
+    let list = []
+    for (let key in giphys){
+      list.push(
+        <ListGiphy
+          key={giphys[key].id}
+          url={giphys[key].images.fixed_width.url}
+          selected
+          handleOnclick={() => this.handleOnclick(giphys[key])}
+        />
+      )
+    }
+    return list
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        {this.listGiphy(this.props.favorites)}
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    favorites: state.favorites
+  }
+}
+
+export default connect(mapStateToProps,{deleteFavorite})(Favorites)
