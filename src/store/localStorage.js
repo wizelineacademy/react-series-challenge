1978d1aafe5d82446656c9a1903e7eb01861288f
bgilm <benjamingil94@gmail.com>
2018-03-12 13:46:13 -0600

    Add local storage persistence functions

---
+++ src/store/localStorage.js
@@ -0,0 +1,21 @@
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state')
+    if(serializedState === null) {
+      return undefined
+    }
+    return JSON.parse(serializedState)
+  } catch(err) {
+    return undefined
+  }
+}
+
+export const saveState = (state) => {
+  console.log(state);
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('state', serializedState)
+  } catch (err) {
+    console.log(err);
+  }
+}
