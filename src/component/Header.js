393b1573bb9f05f5216bcfd78941451429be826c
betoleal <alberto.leal@wizeline.com>
2018-03-12 11:43:21 -0600

    Add style to header component

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+`
+const Li = styled.li`
+  display: inline;
+  padding: 0 10px;
+  border-right: 1px solid black;
+  &:last-child {
+    border-right: 0;
+  }
+`
 
 class Header extends Component {
   render (){
     return (
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/favorites">favorites</Link>
-      </nav>
+      <Nav>
+        <ul>
+          <Li>
+            <Link to="/">Home</Link>
+          </Li>
+          <Li>
+            <Link to="/favorites">Favorites</Link>
+          </Li>
+        </ul>
+      </Nav>
     )
   }
 }
