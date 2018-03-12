fc37ba9475150421a4b236b7c45ed673bafc4e38
Luis Alducin <luis.acevedo@wizeline.com>
2018-03-12 00:13:09 -0600

    :boom: - Adding middlewares and store to the app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './components/Home';
 import registerServiceWorker from './registerServiceWorker';
+import createSagaMiddleware from 'redux-saga';
+import {composeWithDevTools} from "redux-devtools-extension";
+import {applyMiddleware, createStore} from "redux";
+import rootReducer from "./reducers";
+import rootSaga from "./sagas";
+import {Provider} from "react-redux";
+import HomeContainer from "./containers/Home";
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(),
+  applyMiddleware(sagaMiddleware)
+);
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <HomeContainer />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
