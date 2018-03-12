69413b05ef2b804405dddacaa12e8f68272b0435
Luis Alducin <luis.acevedo@wizeline.com>
2018-03-12 00:12:09 -0600

    :sparkles: - Add proptypes to Home component

--- src/components/Home/Home.style.js
+++ src/components/Home/Home.style.js
@@ -3,14 +3,14 @@ import styled from 'styled-components'
 export const HomeWrapper = styled.div`
   display: inline-block;
   height: 100%;
-  margin: 0 200px;
-  width: -moz-available;
-  width: -webkit-fill-available;
+  width: 100%;
  `;
 
 export const SearchWrapper = styled.div`
   display: inline-flex;
-  width: 100%;
+  width: -moz-available;
+  width: -webkit-fill-available;
+  margin: 0 200px;
 `;
 
 export const SearchInput = styled.input`
@@ -20,7 +20,7 @@ export const SearchInput = styled.input`
   display: inline-block;
   font-size: 20px;
   height: 62px;
-  margin: 20px 12px;
+  margin: 50px 12px;
   padding: 0 10px;
   width: 100%;
 
@@ -33,6 +33,14 @@ export const SearchButton = styled.img`
   background: linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%);
   cursor: pointer;
   height: 64px;
-  margin-top: 20px;
+  margin-top: 50px;
   width: 64px;
 `;
+
+export const TrendingGifCards = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  height: 100vh;
+  margin: 0 160px;
+  overflow: auto;
+ `;
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import {HomeWrapper, SearchButton, SearchInput, SearchWrapper} from "./Home.style";
-import searchIcon from './search.png'
+import React from 'react';
+import PropTypes from 'prop-types';
+import {HomeWrapper, SearchButton, SearchInput, SearchWrapper, TrendingGifCards} from './Home.style';
+import searchIcon from './search.png';
+import GifCard from "../GifCard";
 
 class Home extends React.Component {
+  componentWillMount() {
+    this.props.fetchTrendingGifs();
+  }
+
   render() {
     return (
       <HomeWrapper>
@@ -10,9 +16,23 @@ class Home extends React.Component {
           <SearchInput placeholder={'Type to search cool GIFs'} />
           <SearchButton src={searchIcon} />
         </SearchWrapper>
+        <TrendingGifCards>
+          {
+            this.props.trending.map((gif) => {
+              return (
+                <GifCard url={gif.images.fixed_width_downsampled.url}  isFavorite={false} />
+              )
+            })
+          }
+        </TrendingGifCards>
       </HomeWrapper>
     );
   }
 }
+
+Home.propTypes = {
+  fetchTrendingGifs: PropTypes.func.isRequired,
+  trending: PropTypes.array.isRequired
+};
 
 export default Home;
