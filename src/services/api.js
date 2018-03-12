f29bc128343d64ce8e6d76a87ba45e398d5f14f8
bgilm <benjamingil94@gmail.com>
2018-03-12 17:18:32 -0600

    Add api call to service

--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,6 @@ const BASE_URL = 'https://api.giphy.com/v1/gifs/'
 
 export const getTrending = () => fetch(`${BASE_URL}trending?api_key=${API_KEY}&limit=50`)
 .then(res => res.json())
+
+export const getSearch = payload => fetch(`${BASE_URL}search?api_key=${API_KEY}&q=${payload}&limit=50`)
+.then(res => res.json())
