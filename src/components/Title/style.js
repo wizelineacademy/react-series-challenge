5c660f626af9badbb4e692a131980b735faf733e
Rodrigo Guinea <rguinea@Guineas-MacBook-Pro.local>
2018-03-12 09:52:03 -0600

    Add title component

---
+++ src/components/Title/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const BurningEffect = styled.h1`
+  color: #fff;
+  text-shadow: 0px -1px 4px white, 0px -2px 10px yellow, 0px -10px 20px #ff8000, 0px -18px 40px red;
+  font: 2em 'BlackJackRegular';
+`
+
+export const NeonEffect = styled.h1`
+  color: #fff;
+  text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #ff2d95, 0 0 30px #ff2d95, 0 0 40px #ff2d95, 0 0 50px #ff2d95, 0 0 75px #ff2d95;
+  letter-spacing: 5px;
+  font: 3em 'Miso';
+`
