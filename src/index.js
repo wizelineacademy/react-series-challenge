93e38aa299fca2c0c719ec2c63f73d37bbca6db6
Luis Alducin <luis.acevedo@wizeline.com>
2018-03-12 14:55:24 -0600

    :boom: - Add routing to the app

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import rootReducer from "./reducers";
 import rootSaga from "./sagas";
 import {Provider} from "react-redux";
 import HomeContainer from "./containers/Home";
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
@@ -21,7 +21,16 @@ sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
-    <HomeContainer />
+    <Router>
+      <Switch>
+        <Route exact path={'/'} component={HomeContainer} />
+        <Route path={'/favorites'} render={() => (
+          <h2>FAVORITES</h2>
+        )} />
+        <Route path={'/detail/:id'} render={() => (<h2>DETAILS</h2>)} />
+        <Route render={() => (<h2>NOT FOUND</h2>)} />
+      </Switch>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
