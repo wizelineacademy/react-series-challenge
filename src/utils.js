794e6dbbf922830132995934f76b26220c01179e
Jose J. Breijo <josejbreijo99@gmail.com>
2018-03-13 14:24:50 -0600

    Remove unused fn

--- src/utils.js
+++ src/utils.js
@@ -9,21 +9,4 @@ const db = {
   }
 };
 
-const getColor = () => {
-  const colors = [
-    "#55efc4",
-    "#81ecec",
-    "#74b9ff",
-    "#a29bfe",
-    "#dfe6e9",
-    "#fdcb6e",
-    "#fab1a0",
-    "#ff7675",
-    "#fd79a8",
-    "#636e72"
-  ];
-  const index = Math.floor(Math.random() * colors.length);
-  return colors[index];
-};
-
-export { db, getColor };
+export { db };
