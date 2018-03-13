d2a1ffa8906f74998bce6c7524eace98d80699d9
Victor Tortolero <victor.tortolero@wizeline.com>
2018-03-13 17:51:07 -0600

    Run sagas after creating store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../sagas';
 import rootReducer from '../reducers';
 
 const composeEnhancers = composeWithDevTools({});
@@ -15,5 +16,7 @@ const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(...middleware))
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
