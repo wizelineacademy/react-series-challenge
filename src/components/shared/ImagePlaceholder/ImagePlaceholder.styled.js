e7c2a9b350629db931d3384337ecaab5bb3b03c2
Jose J. Breijo <josejbreijo99@gmail.com>
2018-03-13 14:23:53 -0600

    Delete unused image placeholder

--- src/components/shared/ImagePlaceholder/ImagePlaceholder.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const ColorBoxStyled = styled.div`
-  background-color: ${props => props.color};
-  height: ${props => props.height}px;
-  width: 25vw;
-`;
-
-export { ColorBoxStyled };
