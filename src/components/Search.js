659924e5d3bf050e91f6b53365f7ad12f44c272d
Victor Tortolero <victor.tortolero@wizeline.com>
2018-03-13 17:52:04 -0600

    Add Search component

---
+++ src/components/Search.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import GifGallery from './GifGallery';
+
+class Search extends Component {
+  render() {
+    const { loading, gifs } = this.props;
+
+    if (loading) {
+      return <div>Loading...</div>;
+    };
+
+    return (
+      <div>
+        <GifGallery gifs={gifs} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  loading: state.search.loading,
+  gifs: state.search.gifs
+});
+
+export default connect(mapStateToProps)(Search);
