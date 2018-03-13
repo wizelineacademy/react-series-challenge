37050c5af11c4a22188b07b01f466a02e39c7ec8
bgilm <benjamingil94@gmail.com>
2018-03-13 15:57:07 -0600

    add search actions

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,12 +5,17 @@ const REQUEST_SEARCH_GIFS_FAIL = 'REQUEST_SEARCH_GIFS_FAIL'
 const SEARCH_FAVS = 'SEARCH_FAVS'
 const SEARCH_FAVS_SUCCESS = 'SEARCH_FAVS_SUCCESS'
 
+const SEARCH_GIF = 'SEARCH_GIF'
+const SEARCH_GIF_SUCCESS = 'SEARCH_GIF_SUCCESS'
 const requestSearchGifs = (payload = {}) => ({ type: REQUEST_SEARCH_GIFS, payload });
 const requestSearchGifsSuccess = (payload = {}) => ({ type: REQUEST_SEARCH_GIFS_SUCCESS, payload })
 const requestSearchGifsFail = (payload = {}) => ({ type: REQUEST_SEARCH_GIFS_FAIL, payload })
 
 const searchFavs = (payload={}) => ({type: SEARCH_FAVS, payload})
 const searchFavsSuccess = (payload={}) => ({ type: SEARCH_FAVS_SUCCESS, payload })
+
+const searchGif = (payload={}) => ({type: SEARCH_GIF, payload})
+const searchGifSuccess = (payload={}) => ({ type: SEARCH_GIF_SUCCESS, payload })
 
 
 export default {
@@ -19,13 +24,17 @@ export default {
     REQUEST_SEARCH_GIFS_SUCCESS,
     REQUEST_SEARCH_GIFS_FAIL,
     SEARCH_FAVS,
-    SEARCH_FAVS_SUCCESS
+    SEARCH_FAVS_SUCCESS,
+    SEARCH_GIF,
+    SEARCH_GIF_SUCCESS
   },
   creators: {
     requestSearchGifs,
     requestSearchGifsSuccess,
     requestSearchGifsFail,
     searchFavs,
-    searchFavsSuccess
+    searchFavsSuccess,
+    searchGif,
+    searchGifSuccess
   },
 };
