c827cf24117bd7deb9d7ad581b06630cd4b1b27a
bgilm <benjamingil94@gmail.com>
2018-03-13 13:05:51 -0600

    Add search favs actions

--- src/actions/search.js
+++ src/actions/search.js
@@ -2,19 +2,30 @@ const REQUEST_SEARCH_GIFS = 'REQUEST_SEARCH_GIFS';
 const REQUEST_SEARCH_GIFS_SUCCESS = 'REQUEST_SEARCH_GIFS_SUCCESS'
 const REQUEST_SEARCH_GIFS_FAIL = 'REQUEST_SEARCH_GIFS_FAIL'
 
+const SEARCH_FAVS = 'SEARCH_FAVS'
+const SEARCH_FAVS_SUCCESS = 'SEARCH_FAVS_SUCCESS'
+
 const requestSearchGifs = (payload = {}) => ({ type: REQUEST_SEARCH_GIFS, payload });
 const requestSearchGifsSuccess = (payload = {}) => ({ type: REQUEST_SEARCH_GIFS_SUCCESS, payload })
 const requestSearchGifsFail = (payload = {}) => ({ type: REQUEST_SEARCH_GIFS_FAIL, payload })
+
+const searchFavs = (payload={}) => ({type: SEARCH_FAVS, payload})
+const searchFavsSuccess = (payload={}) => ({ type: SEARCH_FAVS_SUCCESS, payload })
+
 
 export default {
   types: {
     REQUEST_SEARCH_GIFS,
     REQUEST_SEARCH_GIFS_SUCCESS,
-    REQUEST_SEARCH_GIFS_FAIL
+    REQUEST_SEARCH_GIFS_FAIL,
+    SEARCH_FAVS,
+    SEARCH_FAVS_SUCCESS
   },
   creators: {
     requestSearchGifs,
     requestSearchGifsSuccess,
-    requestSearchGifsFail
+    requestSearchGifsFail,
+    searchFavs,
+    searchFavsSuccess
   },
 };
