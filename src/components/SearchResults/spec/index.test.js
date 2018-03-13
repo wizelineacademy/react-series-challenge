fe929907b5e43656d592292a9af664e02799db43
Jose J. Breijo <josejbreijo99@gmail.com>
2018-03-13 14:04:05 -0600

    SearchResults tests

---
+++ src/components/SearchResults/spec/index.test.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Provider } from "react-redux";
+import renderer from "react-test-renderer";
+import ReactDOM from "react-dom";
+import configureStore from "redux-mock-store";
+import "jest-styled-components";
+import SearchResults from "../";
+
+const mockStore = configureStore();
+
+const initialState = {
+  searchReducer: {
+    loaded: false,
+    gifs: [
+      {
+        id: 1,
+        images: {
+          original: { width: 400, height: 400, url: "http://original" },
+          fixed_width: { width: 300, height: 300, url: "http://fixed" }
+        }
+      }
+    ]
+  }
+};
+
+describe("<SearchResults />", () => {
+  it("renders properly", () => {
+    const init = Object.assign(initialState);
+    const store = mockStore(init);
+    ReactDOM.createPortal = node => node;
+    const component = renderer.create(
+      <Provider store={store}>
+        <SearchResults />
+      </Provider>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
