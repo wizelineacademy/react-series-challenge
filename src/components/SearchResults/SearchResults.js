ead3d97d01b7955d20bdc6483691ca1fd7e5255b
Jose J. Breijo <josejbreijo99@gmail.com>
2018-03-13 15:07:25 -0600

    Add a message for empty search

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -2,10 +2,18 @@ import React from "react";
 import { connect } from "react-redux";
 
 import TileList from "../shared/TileList";
+import { SearchResultsEMptyStyled } from "./SearchResults.styled";
 
-const SearchResults = ({ gifs, loaded }) => (
-  <TileList gifs={gifs} loading={!loaded} />
-);
+const SearchResults = ({ gifs, loaded }) => {
+  if (loaded && !gifs.length) {
+    return (
+      <SearchResultsEMptyStyled>
+        <p>no gif matched your query, search again</p>
+      </SearchResultsEMptyStyled>
+    );
+  }
+  return <TileList gifs={gifs} loading={!loaded} />;
+};
 
 const mapStateToProps = state => {
   const { gifs, loaded } = state.searchReducer;
