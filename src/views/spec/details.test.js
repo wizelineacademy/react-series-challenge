c6b6e8469603f7a2002222f625adcc7a92a673fe
Alberto Sánchez <krilbert@gmail.com>
2018-03-13 15:55:55 -0600

    Home view testing

--- src/views/spec/details.test.js
+++ src/views/spec/details.test.js
@@ -8,38 +8,94 @@ import { Details } from '../';
 
 const mockStore = configureMockStore();
 
-describe('<Grid />', () => {
+describe('<Details />', () => {
   const props = {
     fetchGif: jest.fn(),
     toggleFavorite: jest.fn(),
     match: { params: { id: 'asdfljlk3-ffgfg' } },
   };
-  const initialState = {
-    favorites: [],
-    gif: {
-      data: {
+
+  test('renders null if no data and not loading', () => {
+    const initialState = {
+      favorites: [],
+      gif: {
+        data: undefined,
+        loading: false,
+      },
+    };
+    const tree = renderer.create(
+      <Provider store={mockStore(initialState)}>
+        <StaticRouter location="" context={{}}>
+          <Details {...props} />
+        </StaticRouter>
+      </Provider>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('renders loader', () => {
+    const initialState = {
+      favorites: [],
+      gif: {
+        data: undefined,
+        loading: true,
+      },
+    };
+    const tree = renderer.create(
+      <Provider store={mockStore(initialState)}>
+        <StaticRouter location="" context={{}}>
+          <Details {...props} />
+        </StaticRouter>
+      </Provider>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('renders error message', () => {
+    const initialState = {
+      favorites: [],
+      gif: {
+        data: undefined,
+        loading: true,
+        error: { message: 'Something went wrong' },
+      },
+    };
+    const tree = renderer.create(
+      <Provider store={mockStore(initialState)}>
+        <StaticRouter location="" context={{}}>
+          <Details {...props} />
+        </StaticRouter>
+      </Provider>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('renders data correctly ', () => {
+    const initialState = {
+      favorites: [],
+      gif: {
         data: {
-          id: 'asdfljlk3-ffgfg',
-          title: 'Example image',
-          images: {
-            original: {
-              url: 'http://www.example.com/image.jpg',
-              width: '800',
-              height: '900',
+          data: {
+            id: 'asdfljlk3-ffgfg',
+            title: 'Example image',
+            images: {
+              original: {
+                url: 'http://www.example.com/image.jpg',
+                width: '800',
+                height: '900',
+              },
+            },
+            user: {
+              username: 'krilbert',
+              display_name: 'Alfer',
+              profile_url: 'http://www.example.com',
+              avatar_url: 'http://www.example.com/avatar.jpg',
             },
           },
-          user: {
-            username: 'krilbert',
-            display_name: 'Alfer',
-            profile_url: 'http://www.example.com',
-            avatar_url: 'http://www.example.com/avatar.jpg',
-          },
         },
+        loading: false,
       },
-    },
-  };
-
-  test('renders data correctly with verified user', () => {
+    };
     const tree = renderer.create(
       <Provider store={mockStore(initialState)}>
         <StaticRouter location="" context={{}}>
