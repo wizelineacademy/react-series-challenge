92878bc36881ffbe9846d0ed4e6d38882aa44afd
betoleal <alberto.leal@wizeline.com>
2018-03-13 12:04:38 -0600

    Add localStorage as global function for testing

--- jest.setup.js
+++ jest.setup.js
@@ -3,3 +3,10 @@ import Adapter from 'enzyme-adapter-react-16';
 import Enzyme from 'enzyme';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+
+const localStorageMock = {
+  getItem: () => '{}',
+  setItem: () => ''
+};
+global.localStorage = localStorageMock
