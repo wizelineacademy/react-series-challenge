708a68822af344b4af1ab48692dba240e2ff87f6
betoleal <alberto.leal@wizeline.com>
2018-03-13 10:20:34 -0600

    Add localstorage to preserve the state for favorites

--- src/reducers/favorites-reducer.js
+++ src/reducers/favorites-reducer.js
@@ -1,13 +1,20 @@
 import {ADDED_FAVORITE, DELETED_FAVORITE} from '../actions/types'
 
-export default function(state={}, action) {
+const initialFavorites = JSON.parse(localStorage.getItem('favorites')) || {}
+
+export default function(state=initialFavorites, action) {
+  let newState
   switch(action.type){
     case ADDED_FAVORITE:
-      return {...state, [action.payload.id]: action.payload}
+      newState = {...state, [action.payload.id]: action.payload}
+      localStorage.setItem('favorites', JSON.stringify(newState))
+      return newState
     case DELETED_FAVORITE:
       const oldState = state
       delete oldState[action.payload.id]
-      return { ...oldState }
+      newState = { ...oldState }
+      localStorage.setItem('favorites', JSON.stringify(newState))
+      return newState
     default:
       return state
   }
