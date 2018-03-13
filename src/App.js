b95b71d02e4bb3d334ff459470a25278905616ae
Victor Tortolero <victor.tortolero@wizeline.com>
2018-03-13 17:52:23 -0600

    Update App component to use react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
+import { Router, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import { Home, SearchBar, Search } from './components';
+import history from './history';
+
+const StyledAppContent = styled.div`
+  width: 960px;
+  margin: 0 auto;
+  margin-top: ${({ theme }) => theme.dimensions.searchBar.height};
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router history={history}>
+        <React.Fragment>
+          <SearchBar />
+          <StyledAppContent>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/search/:query" component={Search} />
+            </Switch>
+          </StyledAppContent>
+        </React.Fragment>
+      </Router>
     );
   }
 }
