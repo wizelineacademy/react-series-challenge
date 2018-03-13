9d8c1495c17652e0cd02b0854fc0058163e38b53
bgilm <benjamingil94@gmail.com>
2018-03-13 15:57:29 -0600

    update reducers index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import trendingGifs from './trending'
 import favoriteGifs from './favorites'
 import searchGifs from './search'
+import activeGif from './activeGif'
 
 export default combineReducers({
   trendingGifs,
   favoriteGifs,
-  searchGifs
+  searchGifs,
+  activeGif
 });
