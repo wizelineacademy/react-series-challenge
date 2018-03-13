a74f94b6043adbd11d50be619e6e19d2ac795a23
bgilm <benjamingil94@gmail.com>
2018-03-12 18:06:02 -0600

    Add functionality to search bar component

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
   StyledInput
 } from './SearchBar.style'
+import { withRouter } from "react-router-dom";
 
-const SearchBar = () => {
-  return(
-    <StyledInput
-      placeholder="Search all the GIFs"
-    />
-  )
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      query:''
+    }
+  }
+
+  onSubmit (event) {
+  event.preventDefault();
+  this.props.searchGifs(this.state.query)
+  this.props.history.push(`/search/${this.state.query}`)
+  }
+
+  render() {
+    return(
+      <form onSubmit={this.onSubmit.bind(this)}>
+        <StyledInput
+          placeholder="Search all the GIFs"
+          value={this.state.query}
+          onChange={event => this.setState({ query: event.target.value })}
+        />
+      </form>
+    )
+  }
+
 }
 
-export default SearchBar
+export default withRouter(SearchBar)
