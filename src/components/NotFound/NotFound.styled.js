4b9bd6975bc38906a9eea6a54a77d70e74030099
Jose J. Breijo <josejbreijo99@gmail.com>
2018-03-13 14:01:28 -0600

    NotFound tests

--- src/components/NotFound/NotFound.styled.js
+++ src/components/NotFound/NotFound.styled.js
@@ -2,10 +2,7 @@ import styled from "styled-components";
 
 const NotFoundWrapperStyled = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
   justify-content: center;
-  height: 90vh;
 `;
 
 export { NotFoundWrapperStyled };
