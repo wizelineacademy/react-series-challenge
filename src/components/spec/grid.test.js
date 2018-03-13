972a2da5febc34d6121a608c692b431dc7888d19
Alberto Sánchez <krilbert@gmail.com>
2018-03-13 13:16:01 -0600

    Tests for Grid component

--- src/components/spec/grid.test.js
+++ src/components/spec/grid.test.js
@@ -1,54 +1,51 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import { StaticRouter } from 'react-router-dom';
+import { Grid } from '../';
 
-import { Loader } from '../';
+const mockStore = configureMockStore();
 
-describe('<Loader />', () => {
+describe('<Grid />', () => {
+  const initialState = { favorites: [] };
+
   test('renders data correctly', () => {
-    const tree = renderer.create(<Loader />).toJSON();
+    const props = {
+      data: [
+        {
+          id: 'asdfljlk3-ffgfg',
+          title: 'Example image',
+          images: {
+            fixed_width_downsampled: { url: 'http://www.example.com/image.jpg' },
+          },
+        },
+      ],
+      toggleFavorite: jest.fn(),
+    };
+
+    const tree = renderer.create(
+      <Provider store={mockStore(initialState)}>
+        <StaticRouter location="" context={{}}>
+          <Grid {...props} />
+        </StaticRouter>
+      </Provider>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('renders data correctly without data', () => {
+    const props = {
+      data: undefined,
+      toggleFavorite: jest.fn(),
+    };
+    const tree = renderer.create(
+      <Provider store={mockStore(initialState)}>
+        <StaticRouter location="" context={{}}>
+          <Grid {...props} />
+        </StaticRouter>
+      </Provider>,
+    ).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
-
-
-// import React from 'react';
-// import renderer from 'react-test-renderer';
-// import configureMockStore from 'redux-mock-store';
-// import { Provider } from 'react-redux';
-// import { StaticRouter } from 'react-router-dom';
-// import { Grid } from '../';
-//
-// const mockStore = configureMockStore();
-//
-// describe('<Grid />', () => {
-//   const props = {
-//     data: [
-//       {
-//         id: 'asdfljlk3-ffgfg',
-//         title: 'Example image',
-//         images: {
-//           fixed_width_downsampled: {
-//             url: 'http://www.example.com/image.jpg',
-//             width: '200',
-//             height: '200',
-//           },
-//         },
-//       },
-//     ],
-//     toggleFavorite: jest.fn(),
-//   };
-//   const initialState = {
-//     favorites: [],
-//   };
-//
-//   test('renders data correctly with verified user', () => {
-//     const tree = renderer.create(
-//       <Provider store={mockStore(initialState)}>
-//         <StaticRouter location="" context={{}}>
-//           <Grid {...props} />
-//         </StaticRouter>
-//       </Provider>,
-//     ).toJSON();
-//     expect(tree).toMatchSnapshot();
-//   });
-// });
