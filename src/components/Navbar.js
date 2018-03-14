48a10ab98502113389fafb8798d3fa42aeccd3a0
Eder Diaz <ederchrono@gmail.com>
2018-03-13 20:57:28 -0600

    💄 styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
+import styled from 'styled-components';
 
 import logo from '../logo.svg';
 
 class Navbar extends Component {
   render () {
     return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-        <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/favs">Favorites</NavLink>
-        </nav>
-      </header>
+      <NavbarStyled>
+        <LogoStyled src={logo} alt="logo" />
+        <TitleStyled>Gif Viewer</TitleStyled>
+        <NavStyled>
+          <NavLinkStyled exact to="/">Home</NavLinkStyled>
+          <NavLinkStyled to="/favs">Favorites</NavLinkStyled>
+        </NavStyled>
+      </NavbarStyled>
     )
   }
 }
 
 export default Navbar
+
+const NavbarStyled = styled.header`
+  height: 72px;
+  box-shadow: 0px 4px 5px 0px rgba(0,0,0,0.5);
+`;
+
+const LogoStyled = styled.img`
+  margin-top: 10px;
+  height: 52px;
+  float: left;
+`
+
+const TitleStyled = styled.h1`
+  float: left;
+`
+const NavStyled = styled.nav`
+  float: right;
+  margin-top: 43px;
+`
+
+const NavLinkStyled = styled(NavLink)`
+  color: #3273dc;
+  padding-bottom: calc(0.5rem - 3px);
+  cursor: pointer;
+  text-decoration: none;
+  line-height: 1.5;
+  margin-right: 30px;
+  &.active {
+    border-bottom-color: #3273dc;
+    border-bottom-style: solid;
+    border-bottom-width: 3px;
+  }
+`
