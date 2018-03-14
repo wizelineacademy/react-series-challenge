9fbf712cdf3e3d49b4c7fc14cbc86c235d63b7be
Victor Tortolero <victor.tortolero@wizeline.com>
2018-03-13 19:10:02 -0600

    Add getFilteredFavorites selector

--- src/selectors/favorites.js
+++ src/selectors/favorites.js
@@ -1,3 +1,16 @@
+import { getQuery } from './search';
+
 export const getFavorites = ({ favorites }) => favorites;
 export const getFavorite = (id, { favorites }) => favorites[id];
 export const isFavorited = (id, { favorites }) => !!getFavorite(id, { favorites });
+
+export const getFavoritesArray = ({ favorites }) => Object.keys(favorites).map(key => favorites[key]);
+
+export const getFilteredFavorites = (state) => {
+  const favorites = getFavoritesArray(state);
+  const query = getQuery(state);
+
+  return favorites.filter(favorite => {
+    return query.length < 0 || favorite.title.includes(query);
+  });
+};
