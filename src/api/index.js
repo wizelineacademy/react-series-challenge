8f3640b3ef4f63789551475e611df8f459b1d58a
Alberto Sánchez <krilbert@gmail.com>
2018-03-14 16:59:38 -0600

    Remove makeRequest helper

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,9 @@
+import axios from 'axios';
+
 import config from '../app.config';
-import makeAxiosRequest from './makeAxiosRequest';
 
 const { url, key } = config.api;
 
-export const loadTrending = () => makeAxiosRequest('get', `${url}/trending`, { api_key: key });
-export const getGif = id => makeAxiosRequest('get', `${url}/${id}`, { api_key: key });
-export const searchGif = text => makeAxiosRequest('get', `${url}/search`, { api_key: key, q: encodeURI(text) });
+export const loadTrending = () => axios.get(`${url}/trending`, { params: { api_key: key } });
+export const getGif = id => axios.get(`${url}/${id}`, { params: { api_key: key } });
+export const searchGif = text => axios.get(`${url}/search`, { params: { api_key: key, q: encodeURI(text) } });
