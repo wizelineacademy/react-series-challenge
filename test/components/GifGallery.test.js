3312d6fa692c623af27f5605728360c4d828f7a9
Victor Tortolero <victor.tortolero@wizeline.com>
2018-03-14 14:16:15 -0600

    Add GifGallery component tests

---
+++ test/components/GifGallery.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import GifGallery from '../../src/components/GifGallery';
+import Gif from '../../src/components/Gif';
+
+describe('<GifGallery />', () => {
+  const gifs = [
+    {
+      id: 1,
+      images: {
+        fixed_width: { url: 'foo.png' }
+      }
+    },
+    {
+      id: 2,
+      images: {
+        fixed_width: { url: 'bar.png' }
+      }
+    }
+  ];
+
+  it('should not render correctly if no gifs are passed to it', () => {
+    const execute = () => {
+      shallow(<GifGallery />);
+    };
+    expect(execute).toThrow();
+  });
+
+  it('should render correctly when passed in an array of gifs', () => {
+    const execute = () => {
+      shallow(<GifGallery gifs={gifs} />);
+    };
+    expect(execute).not.toThrow();
+  });
+
+  it('should render 2 Gif component when passed 2 gifs', () => {
+    const wrapper = shallow(<GifGallery gifs={gifs} />);
+    expect(wrapper.find(Gif)).toHaveLength(2);
+  });
+});
