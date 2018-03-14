c97167b5bc9b21ce1a36aef2f2f748057741b1fe
betoleal <alberto.leal@wizeline.com>
2018-03-13 20:08:23 -0600

    add Header component test cases

---
+++ test/HeaderComponentTest.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {expect} from 'chai'
+import { shallow } from 'enzyme'
+import Header from '../src/component/Header'
+
+describe('Test Header component', () => {
+  let component
+  beforeEach(() => {
+    component = shallow(<Header />)
+  })
+
+  it('shows a component', () => {
+    expect(component).lengthOf(1)
+  })
+
+  it('evaluete links', () => {
+    expect(component.find('Link').at(0).prop('to')).to.be.equal('/')
+    expect(component.find('Link').at(1).prop('to')).to.be.equal('/favorites')
+  })
+
+})
