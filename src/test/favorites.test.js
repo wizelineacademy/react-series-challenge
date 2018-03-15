7495c0ff8d93d84e6893c4da0b39ae515cbe90ef
Eder Diaz <ederchrono@gmail.com>
2018-03-14 22:22:02 -0600

    ✅ add snapshot tests

---
+++ src/test/favorites.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Favorites from '../pages/Favorites'
+import gifData from './data/gifTestData'
+import configureStore from 'redux-mock-store'
+
+const mockStore = configureStore()
+const initialState = {
+  favoriteGifs: [
+    { id: gifData.id }
+  ]
+}
+
+describe('GifTile', () => {
+  let wrapper
+
+  beforeAll(() => {
+    wrapper = shallow(
+      <Favorites store={mockStore(initialState)}/>
+    );
+  })
+
+  it('matches GifTile snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
