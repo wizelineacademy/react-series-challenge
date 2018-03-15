ae593b2393f0ef39dc84d02aa3b0c6b7a4625ee5
Victor Tortolero <victor.tortolero@wizeline.com>
2018-03-14 23:06:14 -0600

    Refactor search to handle when link is set on load

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import GifGallery from './GifGallery';
-import { isSearching, getSearchGifs } from '../selectors/search';
+import { isSearching, getSearchGifs, getQuery } from '../selectors/search';
+import { searchGifsChangeQuery } from '../actions/search';
+import history from '../history';
+import { withRouter } from 'react-router-dom';
 
 export class Search extends Component {
+  componentDidMount() {
+    const { searchGifsChangeQuery, match, stateQuery } = this.props;
+
+    if (stateQuery) {
+      return;
+    }
+
+    const { query } = match.params;
+
+    if (!query) {
+      history.push('/');
+    }
+
+    searchGifsChangeQuery(query);
+  }
+
   render() {
     const { loading, gifs } = this.props;
 
@@ -17,9 +36,16 @@ export class Search extends Component {
   }
 }
 
+const mapDispatchToProps = {
+  searchGifsChangeQuery
+};
+
 const mapStateToProps = state => ({
   loading: isSearching(state),
-  gifs: getSearchGifs(state)
+  gifs: getSearchGifs(state),
+  stateQuery: getQuery(state)
 });
 
-export default connect(mapStateToProps)(Search);
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(Search)
+);
