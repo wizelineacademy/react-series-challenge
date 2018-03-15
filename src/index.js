77b407e8c500cbdf67a17dec7f1ca985a8557bbe
bgilm <benjamingil94@gmail.com>
2018-03-15 16:40:00 -0600

    Refactor routes and unecesasary component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import {
-  BrowserRouter as Router,
-} from 'react-router-dom';
+import store from './store'
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import Routes from './Routes'
+import {
+  StyledTitle
+} from './App.style'
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <div className="App">
+      <StyledTitle>Awesome Gif App</StyledTitle>
+      <Routes />
+    </div>
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
