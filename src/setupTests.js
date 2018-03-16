7b3f352f639579798ce5cdf75da7f65e42107536
Luis Alducin <luis.acevedo@wizeline.com>
2018-03-15 20:15:49 -0600

    :recycle: - Add localStorage tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,25 @@
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme from 'enzyme';
 
+const localStorageMock = (function () {
+  let store = {};
+
+  return {
+    getItem: function (key) {
+      return store[key] || null;
+    },
+    setItem: function (key, value) {
+      store[key] = value.toString();
+    },
+    clear: function () {
+      store = {};
+    }
+  };
+
+})();
+
+Object.defineProperty(global, 'localStorage', {
+  value: localStorageMock
+});
+
 Enzyme.configure({ adapter: new Adapter() });
