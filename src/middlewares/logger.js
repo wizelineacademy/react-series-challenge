678f7c53c9980d6f6aee692906ec62f4779dc111
Pablo Melendez <pablofer.melendez@gmail.com>
2018-03-24 17:10:47 -0500

    create logger middleware

---
+++ src/middlewares/logger.js
@@ -0,0 +1,17 @@
+const logger = (store) => (next) => (action) => {
+  const { type } = action;
+
+  const currentState = store.getState();
+  const continueAction = next(action);
+  const nextState = store.getState();
+
+  console.group(type);
+  console.log(currentState);
+  console.log(action);
+  console.log(nextState);
+  console.groupEnd();
+
+  return continueAction;
+}
+
+export default logger;
