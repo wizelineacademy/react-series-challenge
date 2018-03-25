4a467051418e8a571b85d010c2146cb398ffc26f
José Francisco <francisco@beluga.solutions>
2018-03-24 19:27:23 -0600

    added event handler to get value of the input
    if user press enter in input or press the button

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,12 +3,22 @@ import {SearchButton, SearchInput} from "./SearchBar.styled";
 import {FlexContainer} from "../../shared/components.styled";
 
 export class SearchBar extends Component {
+  searchGifs = (e) => {
+    e.preventDefault();
+    console.log(this.searchInput.value);
+  };
   render() {
     return (
-      <FlexContainer justifyContent='space-around'>
-        <SearchInput type="text" placeholder="Enter something"/>
-        <SearchButton>Search</SearchButton>
-      </FlexContainer>
+      <form onSubmit={this.searchGifs}>
+        <FlexContainer justifyContent='space-around'>
+            <SearchInput
+              type="text"
+              placeholder="Enter something"
+              innerRef={(input) => { this.searchInput = input }}
+            />
+            <SearchButton type="submit">Search</SearchButton>
+        </FlexContainer>
+      </form>
     )
   }
 }
