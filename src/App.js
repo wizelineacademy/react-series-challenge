0921e0d2dd76a332a7dd6a0510f8b9f9bee2d7e9
José Francisco <francisco@beluga.solutions>
2018-03-24 22:41:26 -0600

    update App component, remove loading function and state

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
 import React, { Component } from 'react';
-import {Provider} from "react-redux";
+import {Provider} from 'react-redux';
 import store from './store';
 import {Container} from "./App.styled";
 import {SearchBar} from "./components/SearchBar/SearchBar";
-import {GifList} from "./components/GifList/GifList";
-import {getTrendingGifs} from "./api";
+import GifList from "./components/GifList/GifList";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gifs: []
-    };
-  }
-  componentDidMount() {
-    getTrendingGifs()
-      .then(result => {
-        console.log(result);
-        this.setState({
-          gifs: result.data
-        });
-      });
-  }
   render() {
     return (
       <Provider store={store}>
         <Container>
           <SearchBar />
-          <GifList gifs={this.state.gifs} />
+          <GifList/>
         </Container>
       </Provider>
     );
   }
 }
-
 export default App;
