b86a0ffed51846c6a655a95cf23a4fd6f2df5597
José Francisco <francisco@beluga.solutions>
2018-03-25 16:55:34 -0600

    connected GifList,  added action to GifList, add gif to favorites

--- src/components/GifList/GifList.js
+++ src/components/GifList/GifList.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import {bindActionCreators} from "redux";
+import {connect} from 'react-redux';
 import {Gif} from "../Gif/Gif";
 import {MasonryContainer} from "./GifList.styled";
+import favoritesGifs from "../../actions/favoritesGifs";
 
-const GifList = ({ gifs }) => {
+const GifList = ({ gifs, addGif }) => {
   return (
     <MasonryContainer>
-      { gifs.map(gif => <Gif key={gif.id} gif={gif} />) }
+      { gifs.map(gif => <Gif addGif={addGif} key={gif.id} gif={gif} />) }
     </MasonryContainer>
   )
 };
+const mapDispatchToProps = (dispatch) => {
+  const { addGif } = favoritesGifs.creators;
 
-export default GifList;
+  return bindActionCreators({
+    addGif,
+  }, dispatch);
+};
+export default connect(null, mapDispatchToProps)(GifList);
 
