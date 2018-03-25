b2e5f69d4739c47ddc2de4641dd5721eae81bc56
José Francisco <francisco@beluga.solutions>
2018-03-24 19:41:14 -0600

    added basic Api functions, getTrending and SearchGifs

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+const API_KEY = 'GIPHY-KEY';
+const ENDPOINT = 'http://api.giphy.com';
+
+export const getTrendingGifs = () => {
+  const path = '/v1/gifs/trending';
+  return fetch(`${ENDPOINT}${path}?api_key=${API_KEY}`)
+    .then(res => res.json());
+};
+export const searchGifs = (term) => {
+  const path = '/v1/gifs/search';
+  return fetch(`${ENDPOINT}${path}?q=${term}&api_key=${API_KEY}`)
+    .then(res => res.json());
+};
