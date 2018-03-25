9864c892c599fcd1f13cbdad73dbe380fce8e397
José Francisco <francisco@beluga.solutions>
2018-03-25 16:22:26 -0600

    updated: detail view, make request to get gif by id, connected to store

--- src/components/DetailView/DetailView.js
+++ src/components/DetailView/DetailView.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import {bindActionCreators} from "redux";
+import detailGif from "../../actions/detailGif";
+import {Container} from "../../App.styled";
+import {Gif} from "../Gif/Gif";
+class DetailView extends Component {
+  constructor(props) {
+    super(props);
+    const gifId = props.match.params.gifId;
+    props.getSingleGifRequested(gifId);
+  }
+  render() {
+    const { gif } = this.props.detailGif;
+    return (
+      <Container>
+        <Gif gif={gif} fullWidth={true}/>
+      </Container>
+    )
+  }
+}
 
-const DetailView = () => (
-  <div>
-    Gif detail view
-  </div>
-);
+const mapStateToProps = (state) => {
+  const { detailGif } = state;
+  return { detailGif };
+};
+const mapDispatchToProps = (dispatch) => {
+  const { getSingleGifRequested } = detailGif.creators;
 
-export default DetailView;
+  return bindActionCreators({
+    getSingleGifRequested,
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DetailView);
