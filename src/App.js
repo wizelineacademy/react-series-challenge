01f5bfc2dd058c2ca66df1da9da5fe8cf906b97f
José Francisco <francisco@beluga.solutions>
2018-03-24 22:09:19 -0600

    added Provider tag in App, use store

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {Provider} from "react-redux";
+import store from './store';
 import {Container} from "./App.styled";
 import {SearchBar} from "./components/SearchBar/SearchBar";
 import {GifList} from "./components/GifList/GifList";
@@ -22,10 +24,12 @@ class App extends Component {
   }
   render() {
     return (
-      <Container>
-        <SearchBar />
-        <GifList gifs={this.state.gifs} />
-      </Container>
+      <Provider store={store}>
+        <Container>
+          <SearchBar />
+          <GifList gifs={this.state.gifs} />
+        </Container>
+      </Provider>
     );
   }
 }
