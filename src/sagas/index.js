6f893f549234859079dffdd879be3b84c1fe1f07
José Francisco <francisco@beluga.solutions>
2018-03-25 14:01:12 -0600

    added: searchingGifs saga, get term from state

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,8 +1,10 @@
 import { all } from 'redux-saga/effects';
 import trendingGifsSaga from './trendingGifsSaga';
+import searchingGifsSaga from "./searchingGifsSaga";
 
 export default function* rootSaga() {
   yield all([
-    trendingGifsSaga()
+    trendingGifsSaga(),
+    searchingGifsSaga()
   ]);
 };
