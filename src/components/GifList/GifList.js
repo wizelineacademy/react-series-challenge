e692109f3aff824d32a7ce7dea2737e500dfab2e
José Francisco <francisco@beluga.solutions>
2018-03-24 22:42:18 -0600

    temporal: test trendingGifsSaga, request gifs from GifList

--- src/components/GifList/GifList.js
+++ src/components/GifList/GifList.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import {bindActionCreators} from "redux";
+import { connect } from 'react-redux';
 import {Gif} from "../Gif/Gif";
 import {MasonryContainer} from "./GifList.styled";
+import trendingGifs from '../../actions/trendingGifs';
 
-export class GifList extends Component {
+class GifList extends Component {
+  constructor(props) {
+    super(props);
+    props.getTrendingGifsRequested();
+  }
   render() {
+    const {data} = this.props.trendingGifs;
     return (
       <MasonryContainer>
-        { this.props.gifs.map(gif => <Gif key={gif.id} gif={gif} />) }
+        { data.map(gif => <Gif key={gif.id} gif={gif} />) }
       </MasonryContainer>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  const {
+    trendingGifs,
+  } = state;
+
+  return {
+    trendingGifs,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  const { getTrendingGifsRequested } = trendingGifs.creators;
+
+  return bindActionCreators({
+    getTrendingGifsRequested,
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GifList)
