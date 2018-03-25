c960f0f4db313b108439fed5ff5e8f8ff25d68a4
José Francisco <francisco@beluga.solutions>
2018-03-24 20:44:43 -0600

    updated GifList, use props.gifs to render gifs

--- src/components/GifList/GifList.js
+++ src/components/GifList/GifList.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+import {Gif} from "../Gif/Gif";
+import {MasonryContainer} from "./GifList.styled";
 
 export class GifList extends Component {
   render() {
     return (
-      <ul>
-        <li>Gif</li>
-        <li>Gif</li>
-        <li>Gif</li>
-      </ul>
+      <MasonryContainer>
+        { this.props.gifs.map(gif => <Gif key={gif.id} gif={gif} />) }
+      </MasonryContainer>
     )
   }
 }
---
+++ src/components/GifList/GifList.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const MasonryContainer = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  margin-left: -8px;
+  width: 100%;
+  margin-top: 15px;
+`;
