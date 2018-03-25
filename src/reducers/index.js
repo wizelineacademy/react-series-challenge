bda0dcd613f01ceed8264bf1da962a79fe8aa3e3
José Francisco <francisco@beluga.solutions>
2018-03-25 17:18:08 -0600

    refactor: favorites reducer name in root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
-import favoritesGif from './favoritesGif';
+import favoritesGifs from './favoritesGif';
 import searchingGifs from './searchingGifs';
 import trendingGifs from './trendingGifs';
 import detailGif from './detailGif'
 
 export default combineReducers({
-  favoritesGif,
+  favoritesGifs,
   searchingGifs,
   trendingGifs,
   detailGif
