4d34d5c3f41d35262e8d395220322d19188892a8
José Francisco <francisco@beluga.solutions>
2018-03-25 12:54:09 -0600

    added: Router when rendering main App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from "react-router-dom";
 import App from './App';
 import './styles';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 registerServiceWorker();
