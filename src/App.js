6735b33c298bb34ec5083480e75e07c65408ffe3
José Francisco <francisco@beluga.solutions>
2018-03-24 19:25:31 -0600

    created GifList component, added to App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import {Container} from "./App.styled";
 import {SearchBar} from "./components/SearchBar/SearchBar";
+import {GifList} from "./components/GifList/GifList";
 
 class App extends Component {
   render() {
     return (
       <Container>
         <SearchBar />
-        <div>
-            <p>ListOfGifs</p>
-        </div>
+        <GifList />
       </Container>
     );
   }
---
+++ src/components/GifList/GifList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class GifList extends Component {
+  render() {
+    return (
+      <ul>
+        <li>Gif</li>
+        <li>Gif</li>
+        <li>Gif</li>
+      </ul>
+    )
+  }
+}
