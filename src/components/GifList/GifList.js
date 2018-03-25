56bbce1476e33cacd29c3a5f63d62021c750ae99
José Francisco <francisco@beluga.solutions>
2018-03-25 12:52:42 -0600

    changed GifList to functional component, only display gifs

--- src/components/GifList/GifList.js
+++ src/components/GifList/GifList.js
@@ -1,40 +1,14 @@
-import React, { Component } from 'react';
-import {bindActionCreators} from "redux";
-import { connect } from 'react-redux';
+import React from 'react';
 import {Gif} from "../Gif/Gif";
 import {MasonryContainer} from "./GifList.styled";
-import trendingGifs from '../../actions/trendingGifs';
 
-class GifList extends Component {
-  constructor(props) {
-    super(props);
-    props.getTrendingGifsRequested();
-  }
-  render() {
-    const {data} = this.props.trendingGifs;
-    return (
-      <MasonryContainer>
-        { data.map(gif => <Gif key={gif.id} gif={gif} />) }
-      </MasonryContainer>
-    )
-  }
-}
-
-const mapStateToProps = (state) => {
-  const {
-    trendingGifs,
-  } = state;
-
-  return {
-    trendingGifs,
-  };
-};
-const mapDispatchToProps = (dispatch) => {
-  const { getTrendingGifsRequested } = trendingGifs.creators;
-
-  return bindActionCreators({
-    getTrendingGifsRequested,
-  }, dispatch);
+const GifList = ({ gifs }) => {
+  return (
+    <MasonryContainer>
+      { gifs.map(gif => <Gif key={gif.id} gif={gif} />) }
+    </MasonryContainer>
+  )
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(GifList)
+export default GifList;
+
