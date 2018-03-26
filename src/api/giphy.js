1ff31353149f697d1be7de5b810d3d9d66f9bc3b
Irving Fernando Alvarez Vazquez <irvingav@mx1.ibm.com>
2018-03-25 20:27:23 -0600

    Updated calls to https protocols

--- src/api/giphy.js
+++ src/api/giphy.js
@@ -1,7 +1,7 @@
 export const loadTrending = () => {
 	//console.log('[API] retrieving trending...');
 	return fetch(
-		'http://api.giphy.com/v1/gifs/trending?api_key=1PwdD3bS9JHQDGug0JKcOcvRG7KgovDn&limit=24'
+		'https://api.giphy.com/v1/gifs/trending?api_key=1PwdD3bS9JHQDGug0JKcOcvRG7KgovDn&limit=24'
 	)
 		.then(res => res.json())
 		.then(data => data.data)
@@ -14,7 +14,7 @@ export const loadTrending = () => {
 export const loadSearch = text => {
 	//console.log('[API] retrieving search...');
 	return fetch(
-		'http://api.giphy.com/v1/gifs/search?q=' +
+		'https://api.giphy.com/v1/gifs/search?q=' +
 			text +
 			'&api_key=1PwdD3bS9JHQDGug0JKcOcvRG7KgovDn&limit=24'
 	)
