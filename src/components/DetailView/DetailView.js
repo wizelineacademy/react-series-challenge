03c4116771317ed436ce8b801ad4b697f43c0b66
José Francisco <francisco@beluga.solutions>
2018-03-25 18:57:51 -0600

    fix: detailView, added actions, addGif, removeGift, favorite indication

--- src/components/DetailView/DetailView.js
+++ src/components/DetailView/DetailView.js
@@ -4,6 +4,12 @@ import {bindActionCreators} from "redux";
 import detailGif from "../../actions/detailGif";
 import {Container} from "../../App.styled";
 import {Gif} from "../Gif/Gif";
+import favoritesGifs from "../../actions/favoritesGifs";
+
+const isFavorited = (favorites, gifId) => {
+  return favorites[gifId] !== undefined;
+};
+
 class DetailView extends Component {
   constructor(props) {
     super(props);
@@ -11,23 +17,34 @@ class DetailView extends Component {
     props.getSingleGifRequested(gifId);
   }
   render() {
+    const { addGif, removeGif, favoritesGifs } = this.props;
     const { gif } = this.props.detailGif;
     return (
       <Container>
-        <Gif gif={gif} fullWidth={true} isInDetail={true}/>
+        <Gif
+          gif={gif}
+          fullWidth={true}
+          isInDetail={true}
+          addGif={addGif}
+          removeGif={removeGif}
+          isFavorited={isFavorited(favoritesGifs.allGifs, gif.id)}
+        />
       </Container>
     )
   }
 }
 
 const mapStateToProps = (state) => {
-  const { detailGif } = state;
-  return { detailGif };
+  const { detailGif, favoritesGifs } = state;
+  return { detailGif, favoritesGifs };
 };
 const mapDispatchToProps = (dispatch) => {
   const { getSingleGifRequested } = detailGif.creators;
+  const { addGif, removeGif } = favoritesGifs.creators;
 
   return bindActionCreators({
+    addGif,
+    removeGif,
     getSingleGifRequested,
   }, dispatch);
 };
