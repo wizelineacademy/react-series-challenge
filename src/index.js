9d939b690746c78b1786ce0cc0c0ec2280e8911b
Elbert Castañeda <elbert@sbxretail.com>
2018-03-27 09:56:51 -0600

    Add de GIF Service to the GIPHY SDK implementation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './style/global';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { gif } from './giphy_sdk';
+
+gif.search({q: 'ryan+gosling'})
+  .then(data => {
+    console.log(data, 'search');
+  });
+  gif.trending()
+  .then(data => {
+    console.log(data, 'trending');
+  });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/style/global.js
+++ src/style/global.js
@@ -2,7 +2,6 @@ import { injectGlobal } from 'styled-components';
 
 injectGlobal`
 body {
-  background-color: red;
   margin: 0;
   padding: 0;
   font-family: sans-serif;
