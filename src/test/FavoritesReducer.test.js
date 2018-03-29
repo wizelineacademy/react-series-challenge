362b7b6fd18804d3647eb75c8a1c1a4a62eaffcf
Pablo Melendez <pablofer.melendez@gmail.com>
2018-03-29 17:28:57 -0500

    reducers test

---
+++ src/test/FavoritesReducer.test.js
@@ -0,0 +1,56 @@
+import { SET_FAVORITES }from '../actions/types';
+import reducer from '../reducers/favorites_reducer';
+
+const dataState = {
+  favorites: [
+    {
+      title: 'Emma Watson Gif',
+      images: {
+        original: 'https://www.google.com'
+      }
+    },
+    {
+      title: 'Fast and Furious Gif',
+      images: {
+        original: 'https://www.google.com'
+      }
+    },
+    {
+      title: 'Gal Gadot Gif',
+      images: {
+        original: 'https://www.google.com'
+      }
+    },
+    {
+      title: 'Coding Gif',
+      images: {
+        original: 'https://www.google.com'
+      }
+    },
+    {
+      title: 'Luke Skywalker Gif',
+      images: {
+        original: 'https://www.google.com'
+      }
+    }
+  ]
+}
+
+const cleanState = {
+  favorites: []
+}
+
+describe('test Search Reducer', () => {
+  it('test default case', () => {
+    const result = reducer(dataState,{ type:'NO_ACTION', payload:'123' });
+    expect(result).toEqual(dataState);
+  })
+  it('test clear case', () => {
+    const result = reducer(cleanState,{ type: SET_FAVORITES, payload: dataState.favorites });
+    expect(result).toEqual(dataState);
+  })
+  it('test default case 2', () => {
+    const result = reducer(cleanState,{ type:'NO_ACTION', payload:'123' });
+    expect(result).toEqual(cleanState);
+  })
+})
