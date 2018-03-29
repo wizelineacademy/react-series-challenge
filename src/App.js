ccac8042870e474bf3f07c1414f50eafd7d9693b
Juan Carlos <juan.arias@mobovida.com>
2018-03-28 21:17:00 -0600

    change on app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
-import {
-  NavLink,
-  Redirect,
-  Route,
-  Switch
-} from 'react-router-dom';
 class App extends Component {
   render() {
     return (
       
         <div>
-          <Header>
+          
             <h1>Giphy Challenge</h1>
             <ul>
-              <li><NavLink to="/Home" activeClass="active">Home</NavLink></li>
-              <li><NavLink to="/Favorites" activeClass="active">Favorites</NavLink></li>
+              <li>home</li>
+              <li>favorites</li>
             </ul>
-          </Header>
+          
 
         </div>
       
