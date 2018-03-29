c13446ec574b693f051e17cf59319e3da6764b00
Juan Carlos <juan.arias@mobovida.com>
2018-03-28 21:50:21 -0600

    back to original app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { Route } from 'react-router-dom';
+import store from './store';
+
+
+import TrendingList from './components/TrendingList';
+import SearchResults from './components/SearchResults';
+import SearchBox from './components/SearchBox';
+import FavoriteList from './components/FavoriteList';
+import GiphyDetail from './components/GiphyDetail';
+import Menu from './components/Menu';
+
 class App extends Component {
   render() {
     return (
-      
-        <div>
-          
-            <h1>Giphy Challenge</h1>
-            <ul>
-              <li>home</li>
-              <li>favorites</li>
-            </ul>
-          
-
+      <Provider store={store}>
+        <div className="app-root">
+          <SearchBox/>          
+          <div className="app">
+            
+            <Route path='/' exact component={TrendingList}/>
+            <Route path='/favorites' component={FavoriteList}/>
+            <Route path='/search' component={SearchResults}/>
+            <Route path='/gif/:giphy_id' component={GiphyDetail}/>
+            
+          </div>
+          <Menu/>
         </div>
-      
+      </Provider>
     );
-  }
+  } 
 }
 
 export default App;
