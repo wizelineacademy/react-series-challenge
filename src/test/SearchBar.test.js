3a26f6625173e45f48111ff21f9910ffad800e68
Pablo Melendez <pablofer.melendez@gmail.com>
2018-03-29 23:09:31 -0500

    Fix Warnings

--- src/test/SearchBar.test.js
+++ src/test/SearchBar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import SearchBar from '../components/searchBar';
 import store from '../store'
 
