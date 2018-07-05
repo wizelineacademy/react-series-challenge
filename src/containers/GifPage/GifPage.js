7c4280d906073d9b702f075faae1bdd9e7dd6ac7
jalmarazg <roberto.almaraz@habilgroup.net>
2018-07-05 03:10:53 -0500

    Displays cheeseburguers example using Giphy Search API

--- src/containers/GifPage/GifPage.js
+++ src/containers/GifPage/GifPage.js
@@ -4,16 +4,40 @@ class GifPage extends Component {
     constructor(props){
         super(props);
         this.state = {
-            variable: 1
+            gifs: null
         };
     }
 
+    componentDidMount() {
+        fetch("http://api.giphy.com/v1/gifs/search?api_key=MKGAXNQl5cXUBSBMrXSsufVZ9bqvhX6p&q=cheeseburgers")
+        .then(Response => {
+                return Response.json().then( (json) => {
+                    //console.log(json.data);
+                    this.setState({ gifs: json.data });
+                }
+            );
+        })
+    }
+
     render() {
+        //console.log(this.state.gifs);
+        let arrays = null;
+        
+        if(this.state.gifs !==  null){
+            arrays = Object.keys(this.state.gifs).map((key, index) => {
+            return (<img key = {this.state.gifs[index].id} src = {this.state.gifs[index].images.original.url} alt = "Falla" />);
+            });
+            console.log(this.state.gifs[0]);
+        } else {
+            arrays = <h1>Loading</h1>;
+        }
+
         return(
         <div>
             <header>
                 <nav>
                     <ul>
+                        {arrays}
                         <li>Home</li>
                         <li>Favorites</li>
                     </ul>
