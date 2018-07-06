20ca803be650ea0e0fd67bab2f056639d077c0d6
Alejandro Camarillo <tribak.zero@gmail.com>
2018-07-06 02:21:09 -0500

    feat: Adds home and favorites routes and components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Switch, Route, NavLink } from 'react-router-dom';
+import Home from './components/Home'
+import Favorites from './components/Favorites'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +9,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <header className="App-header">
+            <NavLink to="/">
+              <img src={logo} className="App-logo" alt="jify" />
+            </NavLink>
+            <NavLink to="/favorites">Favorites</NavLink>
+          </header>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/favorites" component={Favorites} />
+            </Switch>
+          </div>
       </div>
     );
   }
