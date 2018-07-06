b46ca8d363e85a949ff2670eb789c5980e432e9d
@RogFed <rogelio@rogfed.ninja>
2018-07-06 13:49:44 -0500

    Import SearchBar and DisplayGifs components responsible for rendering the UI

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -6,11 +6,13 @@ import React from 'react';
 
 //Import Components
 import SearchBar from './home/SearchBar';
+import DisplayGifs from './home/DisplayGifs';
 
 const HomeView = () => {
     return (
         <section className="HomeView">
             <SearchBar />
+            <DisplayGifs />
         </section>
     );
 }
