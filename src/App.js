c74cd07c8010cfdd3a6243a4aee58e5a543ef1f8
Francisco Rafael Arce García <pakoconvans@gmail.com>
2018-07-06 19:21:27 -0500

    modificiacaion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import Routes from "./Routes";
+import { createStore } from "redux";
 import Navbar from "./components/Navbar";
+import reducers from "./reducers";
+const initialState = { tech: "React " };
+const store = createStore(reducers, initialState);
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <h1>{store.getState().tech}</h1>
         <Navbar />
         <Routes />
       </React.Fragment>
