cbef5332126401306ab164e20dfe1681bc91369d
Fernando Gomez <gomezhyuuga@gmail.com>
2018-07-07 13:05:56 -0500

    Adding CSS Normalize

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import './normalize.css';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
+    <App />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
