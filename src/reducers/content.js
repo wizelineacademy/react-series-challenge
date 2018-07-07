efb80bb9aac22ef45d917cc5c24f5c45554b49b9
Andre Cortes <andre.crts@gmail.com>
2018-07-07 00:37:15 -0500

    [add] Create content reducers

--- src/reducers/content.js
+++ src/reducers/content.js
@@ -1,8 +1,24 @@
+import contentActions from '../actions/content'
 
 const initialState = {}
+const { types } = contentActions
 
 const contentReducer = (state = initialState, action) => {
-  return state
+  const { type, payload } = action
+
+  switch(type) {
+    case types.CONTENT_START_LOADING:{
+      return {...state, loading: true}
+    }
+    case types.CONTENT_FETCHED: {
+      return {...state, content: payload, loading:false}
+    }
+    case types.CONTENT_FETCHED_ERROR: {
+      return {...state, loading:false}
+    }
+    default:
+      return state
+  }
 }
 
 export default contentReducer
