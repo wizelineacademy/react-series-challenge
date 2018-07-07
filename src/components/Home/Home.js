4a47cf7b101ef1b5d244cfea8a4157f137ac1bfd
Andre Cortes <andre.crts@gmail.com>
2018-07-07 14:31:02 -0500

    [add] Show trending gifs

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,15 +3,31 @@ import { connect } from 'react-redux'
 import contentActions from '../../actions/content'
 import Header from '../Header'
 import Searchbar from '../Searchbar'
+import Item from '../Item'
 
 const {
   startLoading,
 } = contentActions.actions
 
+const Content = ({data, onClick}) => {
+  const loading = !data
+  if(loading){
+    return(
+      <div> loading ... </div>
+    )
+  }
+  return data.data.map((key, index) => (
+    <Item key={index}
+      imageURL={key.images.original.webp}
+    />
+  ))
+
+}
+
 class Home extends Component {
 
   componentDidMount() {
-    this.props.dispatch(startLoading())
+    this.props.fetchData()
   }
 
   render() {
@@ -19,10 +35,26 @@ class Home extends Component {
       <div>
         <Header/>
         <Searchbar/>
+        <Content
+          data={this.props.content.content}/>
         Home
       </div>
     )
   }
 }
 
-export default connect(state => state)(Home)
+const mapStateToProps = (state) => {
+  return {
+    content: state.content
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchData: () => {
+      dispatch(startLoading())
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
---
+++ src/components/Item/Item.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Item = ({imageURL}) => {
+  return(
+    <img src={imageURL} />
+  )
+}
+
+export default Item
