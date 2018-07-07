99d3a42a036ca382d7b421f78ea522348be574d8
Moises <moises.pedraza.contractor@bbva.com>
2018-07-07 15:42:11 -0500

    Feat  Add searchBar component

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,35 @@
+import { bindActionCreators } from 'redux';
+import search from '../../actions/search'
+import { connect } from 'react-redux';
+import { debounce } from '../../utils'
+import React from 'react'
+import './index.css'
+
+const SearchBar = ({ placeholder, onSearch }) => {
+    /**
+     * 
+     */
+    const debouncedInput = debounce(onSearch, 2000)
+    const onInput = event => debouncedInput(event.target.value);
+    /**
+     * 
+     */
+    return(
+        <section className="bar" >
+            <input className="search" type="search" placeholder={placeholder} onInput={onInput} />
+        </section>
+    )
+}
+
+const mapDispatchToProps = dispatch => {
+    /**
+     * 
+     */
+    const { getSearch: onSearch } = search.creators
+    /**
+     * 
+     */
+    return bindActionCreators({ onSearch }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar) 
