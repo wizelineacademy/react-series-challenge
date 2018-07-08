3e84c8d41f7ae00aebf766cf279a55d1eb502ad1
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-07-08 11:51:29 -0500

    middlewares added

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,11 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers';
+import logger from '../middlewares/logger';
 
-const store = createStore(rootReducer);
+const middlewares = [logger];
+
+const appliedMiddleware = applyMiddleware(...middlewares);
+
+const store = createStore(rootReducer, appliedMiddleware);
 
 export default store;
