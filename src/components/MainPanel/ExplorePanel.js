3191f1e31f4ba21cb13da9cdaafe5f0a51417180
EC-AI <35151322+EC-AI@users.noreply.github.com>
2018-07-08 00:39:39 -0500

    Reafactored to make a more generic and reusable GifListView component

--- src/components/MainPanel/ExplorePanel.js
+++ src/components/MainPanel/ExplorePanel.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import GifListView from "./GifListView";
 import SearchBar from './SearchBar';
 import ErrorBoundary from "../ErrorBoundary";
+import { bindActionCreators } from "redux";
+import actions from "../../actions";
+import { connect } from "react-redux";
 
-const ExplorePanel = () => (
-  <div>
-    <SearchBar />
-    <ErrorBoundary message="Unable to load.">
-      <GifListView />
-    </ErrorBoundary>
-  </div>
+const { getTrendingGifs } = actions.creators;
+
+class ExplorePanel extends Component {
+
+  componentDidMount() {
+    this.props.getTrendingGifs();
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <ErrorBoundary message="Unable to load.">
+          <GifListView data={this.props.data} />
+        </ErrorBoundary>
+      </div>
+    )
+  }
+
+}
+
+const mapStateToProps = state => {
+  const data = state.searchQuery? state.searchResults : state.trendingGifs;
+  return {
+    data
+  }
+}
+
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({getTrendingGifs}, dispatch)
 )
 
-export default ExplorePanel;
+export default connect(mapStateToProps, mapDispatchToProps)(ExplorePanel);
--- src/components/MainPanel/GifListView.js
+++ src/components/MainPanel/GifListView.js
@@ -1,36 +1,15 @@
 import React, { Component } from 'react';
 import GifView from './GifView';
-import { connect } from "react-redux";
-import { bindActionCreators } from 'redux';
 
-import actions from "../../actions";
-const { getTrendingGifs } = actions.creators;
-
-class GifListView extends Component {
-
-  componentDidMount() {
-    this.props.getTrendingGifs();
-  }
-  
-  render() {
-    const {data, getTrendingGifs} = this.props;
-    const gifs = data.map((gif) => (
-      <GifView src={gif.url} key={gif.id} />
-    ))
-    return (
-      <div style={{width: '100%'}}>
-        {gifs}
-      </div>
-    )
-  }
+const GifListView = ({data}) => {
+  const gifs = data.map((gif) => (
+    <GifView src={gif.url} key={gif.id} />
+  ))
+  return (
+    <div style={{width: '100%'}}>
+      {gifs}
+    </div>
+  )
 }
 
-const mapStateToProps = state => ({
-  data: state.trendingGifs
-})
-
-const mapDispatchToProps = dispatch => (
-  bindActionCreators({getTrendingGifs}, dispatch)
-)
-
-export default connect(mapStateToProps, mapDispatchToProps)(GifListView);
+export default GifListView;
