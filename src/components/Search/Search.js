ef30337971fecc6ca0363843847b013b044dbeca
OscarGrande <oscar_obgg@hotmail.com>
2018-07-08 14:32:55 -0500

    the search component is extracted to an external component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import PropTypes from "prop-types";
+
 // actions
-import { searchGif, addFavorites, getFavorites } from "./../../actions";
+import { searchGif } from "./../../actions";
 
 import UI from './Search.ui';
 
 class Search extends Component {
+  static contextTypes = {
+    router: PropTypes.object,
+  };
+
   constructor(...props){
     super(...props);
     this.state = {
@@ -14,15 +19,6 @@ class Search extends Component {
         searchField: '',
       },
     };
-  }
-
-  componentDidMount() {
-    const favoritesList = JSON.parse(localStorage.getItem('favorites'));
-    if(favoritesList && favoritesList.length) {
-      for(let i = 0, len = favoritesList.length; i < len; i += 1){
-        this.props.getFavorites(favoritesList[i]);
-      }
-    }
   }
 
   handleChange = (e, data) => {
@@ -34,13 +30,10 @@ class Search extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
+    this.context.router.history.push(`/search/${this.state.searchForm.searchField}`);
     this.props.searchGif(this.state.searchForm.searchField)
   }
 
-  favorites = (e, obj) => {
-    e.preventDefault();
-    this.props.addFavorites(obj);
-  }
 
   render() {
 
@@ -49,9 +42,6 @@ class Search extends Component {
         handleChange={this.handleChange}
         inrernalData={this.state.searchForm}
         handleSubmit={this.handleSubmit}
-        data={this.props.data}
-        favorites={this.favorites}
-        favoritesList={this.props.favorites}
       />
     );
   };
@@ -72,13 +62,9 @@ Search.propTypes = {
 
 Search.defaultProps = {
   searchGif: () => {},
-  addFavorites: () => {},
-  getFavorites: () => {},
 }
   
 
 export default connect(mapStateToProps, {
   searchGif,
-  addFavorites,
-  getFavorites
 })(Search);
--- src/components/Search/Search.ui.js
+++ src/components/Search/Search.ui.js
@@ -1,35 +1,6 @@
 import React from "react";
 
-const SearchUI = ({ handleChange, inrernalData, handleSubmit, data, favorites, favoritesList }) => {
-  let gifCatalog;
-
-  if(Object.keys(data).length) {
-    gifCatalog = data.data.map((v) => {
-      const index = v.id;
-      const check = (favoritesList || []).some(({ id }) => id === index);
-
-      return(
-        <div key={index} className="row">
-          <div className="col">
-            <div className="gif">
-              <div className="product-image">
-                <img src={v.images.fixed_height_small.url} alt=""/>
-              </div>
-            </div>
-          </div>
-          <button onClick={(e) => {
-                    favorites(e, {
-                      url: v.images.fixed_height_small.url,
-                      id: v.id
-                    })
-                  }}>
-                  Agregar a favoritos
-          </button>
-          <span>{(check ? 'favorito' : '')}</span>
-        </div>
-      );
-    })
-  }
+const SearchUI = ({ handleChange, inrernalData, handleSubmit }) => {
 
   return (
     <div>
@@ -43,7 +14,6 @@ const SearchUI = ({ handleChange, inrernalData, handleSubmit, data, favorites, f
         />
         <button type="submit">Realizar busqueda</button>
       </form>
-      {gifCatalog}
     </div>
   );
 };
