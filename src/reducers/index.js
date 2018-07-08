4d001863e886c6c1d2165b7a06a4d2cd822f1333
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-07-08 17:18:02 -0500

    favorites reducer added to index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import itemsList from './itemsList';
 import loading from './loading';
 import search from './search';
+import favorites from './favorites';
 
 export default combineReducers({
   itemsList,
   loading,
   search,
+  favorites,
 });
