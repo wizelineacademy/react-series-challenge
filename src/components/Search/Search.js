82617ec6319c172ca86842b610c39bfc767ddc34
OscarGrande <ozzkr.obgg@gmail.com>
2018-07-07 22:13:22 -0500

    first version of the view

---
+++ src/components/Search/Search.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import UI from './Search.ui';
+
+class Search extends Component {
+    render() {
+        return(
+            <UI />
+        );
+    };
+};
+
+
+export default Search;
---
+++ src/components/Search/Search.ui.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SearchUI = () => {
+    return (
+        <div>
+            <span>Escribe el nombre de un gif</span>
+            <input id="search" type="text" />
+            <button>Realizar busqueda</button>
+        </div>
+    );
+};
+
+export default SearchUI;
