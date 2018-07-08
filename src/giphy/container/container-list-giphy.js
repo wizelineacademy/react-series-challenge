45f313a7bf04b8f309468b82ff9b84c9e69b7892
Nancy Zitle Juarez <nancy@MacBook-Pro-de-Nancy.local>
2018-07-08 03:54:10 -0500

    get data of search

--- src/giphy/container/container-list-giphy.js
+++ src/giphy/container/container-list-giphy.js
@@ -5,19 +5,14 @@ import Search from '../components/search';
 // import ContainerModal from '../../modal/container/container-modal';
 // import Modal from '../../modal/component/modal';
 
-const GphApiClient = require('giphy-js-sdk-core');
-const client = GphApiClient('STKCOqnN3DKDQJRPbv5il9egmGwZqRSn');
-
 const listGiphy = [];
 export default class ContainerListGiphy extends Component{
   state = {
     dataGiphy:{},
     modalVisible:false,
-    colorHeart:'#777171',
     isFavorite:false,
     colorHeart:'gray'
   }
-
   handleCloseModal = ()=>{
     this.setState({
       modalVisible:false
@@ -29,7 +24,6 @@ export default class ContainerListGiphy extends Component{
     })
   }
   favorites = () =>{
-    // alert('te gusta');
     this.setState({
       isFavorite:!this.state.isFavorite,
       colorHeart:this.state.isFavorite ? 'red' : 'gray'
@@ -44,17 +38,6 @@ export default class ContainerListGiphy extends Component{
         dataGiphy:resultsGiphy
       })
     })
-
-
-    // prueba
-    client.trending("gifs", {})
-    .then((response) => {
-      console.log(response, 'search')
-    })
-    .catch((err) => {
-
-    })
-    // fin prueba
   }
   
   render(){
