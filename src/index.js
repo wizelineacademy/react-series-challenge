e83b8ae9c573850d661e0624771885f2ae6c4826
gemox94 <gerardo.mx.rz@gmail.com>
2018-07-07 20:05:30 -0500

    Set up react router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import './index.css';
+
+import App from './App';
 import store from './store';
 
+import registerServiceWorker from './registerServiceWorker';
+
+
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>,
     document.getElementById('root')
 );
 registerServiceWorker();
