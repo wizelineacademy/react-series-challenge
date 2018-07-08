328279c8749ec4028f8a3dc0d57425d56c093887
Fernando Gomez <gomezhyuuga@gmail.com>
2018-07-08 12:53:59 -0500

    Initial Redux markup: Wrapping app into Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import * as React from 'react';
-import './App.css';
-
+import {BrowserRouter as Router} from 'react-router-dom';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { Provider} from 'react-redux';
+
+import store from './store';
 import ROUTES from './routes';
 import Header from './components/presentational/Header';
 import { Container } from './components/presentational/Layout';
 
+import './App.css';
+
 class App extends React.Component {
   public render() {
     return (
-      <Container>
-        <Header />
-        <section>
-        <Switch>
-          {ROUTES.map(({path, component, exact}) =>
-            <Route key={path} path={path} exact={exact} component={component} />)}
-          <Redirect to='/home' />
-        </Switch>
-        </section>
-      </Container>
+      <Provider store={store}>
+        <Router>
+          <Container>
+            <Header />
+            <section>
+            <Switch>
+              {ROUTES.map(({path, component, exact}) =>
+                <Route key={path} path={path} exact={exact} component={component} />)}
+              <Redirect to='/home' />
+            </Switch>
+            </section>
+          </Container>
+        </Router>
+      </Provider>
     );
   }
 }
-
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 import './normalize.css';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <App />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
