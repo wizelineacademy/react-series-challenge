fcba50717fa6424f2fa9633eeb62ad960aafcf4d
OscarGrande <ozzkr.obgg@gmail.com>
2018-07-07 22:08:53 -0500

    First version of the store is added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+
 import logo from './logo.svg';
 import './App.css';
+
+// Store
+import store from "./store";
+
+// Components
+import Search from "./components/Search/Search";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Search />
+        </div>
+      </Provider>
     );
   }
 }
