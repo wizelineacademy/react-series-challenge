7ea9c68b5872cef9f51f172b86528a5a4ef61327
Fernando Gomez <gomezhyuuga@gmail.com>
2018-07-08 16:55:42 -0500

    Favorite View completed
    Just convert the favs object into an array and reuse GIFList. The Fav button also works out-of-the-box :)

--- src/components/FavsView.tsx
+++ src/components/FavsView.tsx
@@ -1,5 +1,29 @@
 import * as React from 'react';
+import { IGIF } from '../types/gif';
+import GIFList from './presentational/GIFList';
+import {connect} from 'react-redux';
+import { Dispatch, bindActionCreators } from 'redux';
 
-const FavsView = (props: any) => <h1 className="center">Favs View</h1>
+interface IFavsView {
+    gifs: IGIF[]
+}
 
-export default FavsView;
+class FavsView extends React.Component<IFavsView, any> {
+    public render() {
+        return (<div>
+            <h1 className="center">Your favorite GIFS</h1>
+            <GIFList gifs={this.props.gifs} />
+        </div>);
+    }
+}
+
+function mapStateToProps(state: any) {
+    const { favs } = state;
+    const gifs = Object.keys(favs).map(key => favs[key]);
+
+    return { gifs }
+}
+function mapDispatchToProps(dispatch: Dispatch) {
+    return bindActionCreators({}, dispatch);
+}
+export default connect(mapStateToProps, mapDispatchToProps)(FavsView);
