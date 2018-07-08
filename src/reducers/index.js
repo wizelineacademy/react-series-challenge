93f042c8a9dea701c5e053e5ba227baae9b482e1
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-07-08 16:58:24 -0500

    Search reducer added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import itemsList from './itemsList';
+import loading from './loading';
+import search from './search';
 
 export default combineReducers({
   itemsList,
+  loading,
+  search,
 });
