8571414bef7ba9672d87cd793c7060d033e5faf9
EC-AI <35151322+EC-AI@users.noreply.github.com>
2018-07-08 15:37:47 -0500

    Changed source of gif to 'fixed_height_downsampled' to improve loading speed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,14 +8,16 @@ const rootReducer = (state = initialState, action) => {
     case TRENDING_FETCHED:
       var newState = { ...state };
       newState['trendingGifs'] = action.payload.data.map((gif) => ({
-        url: gif.images.original.url,
+        url: gif.images.fixed_height_downsampled.url,
+        original: gif.images.original.url,
         id: gif.id
       }));
       return newState;
     case SEARCH_FETCHED:
       var newState = { ...state };
       newState['searchResults'] = action.payload.data.map((gif) => ({
-        url: gif.images.original.url,
+        url: gif.images.fixed_height_downsampled.url,
+        original: gif.images.original.url,
         id: gif.id
       }));
       newState['searchQuery'] = action.payload.query;
