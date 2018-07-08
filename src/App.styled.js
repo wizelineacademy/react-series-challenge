7bebeb88a54280743646b48259ee1f11cb9f42da
gemox94 <gerardo.mx.rz@gmail.com>
2018-07-08 12:04:54 -0500

    Fixed some styles on navbar and main container

--- src/App.styled.js
+++ src/App.styled.js
@@ -4,7 +4,8 @@ const Navbar = styled.div`
     display: block;
     position: fixed;
     top: 0;
-    width: 98%;
+    left: 0;
+    right: 0;
     background-color: #fff;
     padding: 10px 15px;
     -webkit-box-shadow: 0px 4px 25px -10px rgba(66,66,66,1);
--- src/components/MainGifsView/MainGifsView.styled.js
+++ src/components/MainGifsView/MainGifsView.styled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 const MainGifsViewContainer = styled.div`
+    margin-top: 8rem;
     width: 100%;
     background-color: #fff;
 `;
