c4a0e5a4d4ae1acccfd9d3bd5f75114221a7c59a
Lvcios <lfloresg0801@gmail.com>
2018-07-08 18:55:40 -0500

    component buttons

--- src/components/GifContainer/gifContainer.js
+++ src/components/GifContainer/gifContainer.js
@@ -1,15 +1,18 @@
 import React, {Component} from 'react'
 import {AddFavButton, RemoveFavButton} from '../ActionButtons'
 import heart from '../../assets/heart.png'
-import plus from '../../assets/plus.png'
-import remove from '../../assets/remove.png'
 
 class GifContainer extends Component{
     render(){
-        // let actionButton 
-        // if(this.props.isFav === -1){
-        //     actionButton  = this.props.action === 'add' ? <AddFavButton data-id={this.props.id}/>: <RemoveFavButton data-id={this.props.id}/>
-        // }
+        let actionButton 
+        if(this.props.isFav === -1){
+            if(this.props.action === 'add'){
+                actionButton = <AddFavButton  id={this.props.id}/>
+            }
+            else{
+                actionButton = <RemoveFavButton  id={this.props.id}/>
+            }
+        }
         return <div>
                     <img src={this.props.url} alt={this.props.title} key={'trending-' + this.props.id}/>
                     <br/>
@@ -17,20 +20,7 @@ class GifContainer extends Component{
                     <br/>
                     <label>{this.props.title}</label>
                     <br/>
-                    
-                    {
-                        this.props.isFav === -1 ? 
-                        <button className='btn-action' onClick={this.props.clickFuncion} data-id={this.props.id}>
-                            {
-                                this.props.action === 'add' ? 
-                                <div><img src={plus} alt='add to favs' data-id={this.props.id} height={100} /><br/><label>Add to favs</label></div>:
-                                <div><img src={remove} alt='remove from favs' data-id={this.props.id} height={100}/><br/><label>Remove</label></div>
-                            }
-                        </button>
-                        :
-                        null
-                    }
-                    
+                    {actionButton}
                     <hr/>
 </div>
     }
