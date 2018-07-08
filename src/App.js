252e128eddbe964ac9733848bd149cbfa9c1f501
Daniel Garcia <pel_daniel@hotmail.com>
2018-07-07 21:44:08 -0500

    Remove import logo from App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import Header from './Header';
 import SearchBar from './SearchBar';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
