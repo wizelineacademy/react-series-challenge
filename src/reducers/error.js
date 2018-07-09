558235e8cec55a1b30d698f03e6d0bd67ffeaf14
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-07-08 20:39:01 -0500

    error reducer added

---
+++ src/reducers/error.js
@@ -0,0 +1,16 @@
+import { ERROR_STATUS } from '../actions/types';
+
+const initialState = {
+  err: false,
+  info: {},
+};
+
+export default function(state = initialState, { type, payload }) {
+  switch (type) {
+    case ERROR_STATUS:
+      const newState = { ...payload };
+      return newState;
+    default:
+      return state;
+  }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import itemsList from './itemsList';
 import loading from './loading';
 import search from './search';
 import favorites from './favorites';
+import error from './error';
 
 export default combineReducers({
   itemsList,
   loading,
   search,
   favorites,
+  error,
 });
