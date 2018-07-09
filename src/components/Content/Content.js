1038cffb237bb24ae01c21306a20bdf97cc0632e
Andre Cortes <andre.crts@gmail.com>
2018-07-08 23:30:23 -0500

    [add] Add a portal when selecting a gif

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Item from '../Item'
+import { createPortal } from 'react-dom'
+import './content.css'
 
-const Content = ({data = {}, isLoading = false, onClickFav, favorites, filter}) => {
-  if(isLoading){
-    return(
-      <div> loading ... </div>
-    )
+const ContentList = ({data = {}, onClickFav, favorites, filter, onClick}) => {
+
+  const isEmptyObject =  (obj) => {
+    for(var prop in obj) {
+        return false
+    }
+    return true
   }
 
   if(data.hasOwnProperty('data')){
@@ -14,22 +18,86 @@ const Content = ({data = {}, isLoading = false, onClickFav, favorites, filter}) 
         item={item}
         onClickFav={onClickFav}
         isFav={favorites[item.id] ? true : false}
+        onClick={onClick}
       />
     ))
   } else {
-
-  return Object.keys(data).map((key, index) => {
-    if(data[key].title.search(new RegExp(filter, 'i')) > -1){
-        return(
+  return !isEmptyObject(data) ? Object.keys(data).map((key, index) => {
+    return data[key].title.search(new RegExp(filter, 'i')) > -1 ?
+      (
           <Item key={index}
             item={data[key]}
             onClickFav={onClickFav}
             isFav={favorites[key] ? true : false}
+            onClick={onClick}
           />
-        )
-      }
-      return null
-    })
+      ) : null
+  }) : <div> No Favorites </div>
+  }
+
+}
+
+const PortalContainer = ({children}) => {
+  const nodeToAppend = document.getElementsByTagName('body')[0]
+
+  return createPortal(children, nodeToAppend)
+}
+
+const Portal = ({open, onClick, item}) => {
+    return open ? (
+      <PortalContainer>
+        <div className="portal">
+          <button onClick={onClick} className="btn-close"> Close </button>
+          <img src={item.images.original.webp} alt={item.title}/>
+          <p> {item.title} </p>
+        </div>
+        </PortalContainer>
+    )
+  : null
+}
+
+class Content extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { open: false , item: ''}
+    this.togglePortal = this.togglePortal.bind(this)
+  }
+
+  togglePortal(item = null) {
+    const { open } = this.state
+    if(open){
+      this.setState({open: !open})
+    } else {
+      this.setState({ open: !open, item})
+    }
+  }
+
+  render() {
+    const {data = {}, isLoading = false, onClickFav, favorites, filter} = this.props
+    if(isLoading){
+      return(
+        <div> loading ... </div>
+      )
+    }
+
+    return(
+      <div className="content">
+        <ContentList
+          data={data}
+          favorites={favorites}
+          onClickFav={onClickFav}
+          filter={filter}
+          onClick={this.togglePortal}
+        />
+        <Portal
+          open={this.state.open}
+          onClick={this.togglePortal}
+          item = {this.state.item}
+        />
+      </div>
+    )
+
   }
 }
 
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,13 +3,13 @@ import favIcon from '../../assets/fav.svg'
 import noFavIcon from '../../assets/no-fav.svg'
 import './item.css'
 
-const Item = ({item, onClickFav, isFav}) => {
+const Item = ({item, onClickFav, isFav, onClick}) => {
 
   const favImage = isFav ? favIcon : noFavIcon
   return(
     <div className="contentItem">
       <img src={favImage} className="favButton" onClick={() => onClickFav(item, isFav)} alt="Favorite"/>
-      <img src={item.images.original.webp} className="imgContent" alt={item.title}/>
+      <img src={item.images.original.webp} className="imgContent" alt={item.title} onClick={() => onClick(item)}/>
     </div>
   )
 }
