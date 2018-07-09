3ddd455946719bf0633fba9225fd274232329af2
Moises <moises.pedraza.contractor@bbva.com>
2018-07-08 20:50:42 -0500

    Fix  Get out loading component from gifs

---
+++ src/components/Loading/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+    return (
+        <section className="empty" >
+            <img src="https://media.giphy.com/media/322Gg7gspi2HlkuqD8/giphy.gif" alt="Loading" />
+        </section>
+    )
+}
