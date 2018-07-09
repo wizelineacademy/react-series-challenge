3d7fe33dc08bb25cf645f131e23105c2bf62d7d0
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-07-08 23:42:25 -0500

    Basic function

--- src/components/Gifs/GifList/GifList.js
+++ src/components/Gifs/GifList/GifList.js
@@ -2,11 +2,29 @@ import React from 'react';
 import GifCard from '../GifCard/GifCard'
 import './GifList.css'
 
-const GifList = ({data}) => {
+const GifList = ({data, addFavorites}) => {
+  let gifs
 
-  const gifs = data.map((gif, index) => (
-    <GifCard src={gif.images.downsized.url} key={index}/>
-  ))
+  if (data) {
+    gifs = data.map((gif, index) => {
+      const {id, images, title} = gif
+      const dataGif = {
+        id,
+        images,
+        title
+      }
+
+      return (
+        <GifCard
+          gif={dataGif}
+          key={dataGif.id}
+          addFavorites={addFavorites}
+        />
+      )
+    })
+  } else {
+    gifs = 'No favorites'
+  }
 
   return (
     <section className="GifList">
--- src/components/Gifs/index.js
+++ src/components/Gifs/index.js
@@ -5,7 +5,7 @@ import './Gif.css';
 const Gifs = (props) => (
   <main className="GifContainer">
     <h2>{props.title}</h2>
-    <GifList data={props.gifs}/>
+    <GifList data={props.gifs} addFavorites={props.addFavorites}/>
   </main>
 );
 
