2ae71edecca74a2dc4257536136600f02707b5d2
Daniel Garcia <pel_daniel@hotmail.com>
2018-07-08 21:27:17 -0500

    Fix typos in gifActions

--- src/actions/gifActions.js
+++ src/actions/gifActions.js
@@ -1,8 +1,8 @@
 import { createAction } from 'redux-actions';
 
-const SEARCH_GIFS_FAILED = 'TRENDING_GIFS_FAILED';
-const SEARCH_GIFS_GET = 'TRENDING_GIFS_GET';
-const SEARCH_GIFS_RECEIVED = 'TRENDING_GIFS_RECEIVED';
+const SEARCH_GIFS_FAILED = 'SEARCH_GIFS_FAILED';
+const SEARCH_GIFS_GET = 'SEARCH_GIFS_GET';
+const SEARCH_GIFS_RECEIVED = 'SEARCH_GIFS_RECEIVED';
 
 const failedSearchGifs = createAction(SEARCH_GIFS_FAILED);
 const receivedSearchGifs = (payload = {}) => ({ type: SEARCH_GIFS_RECEIVED, payload });
