b43e3aad5065627ac50c459adfe2461c1d5d5af5
OscarGrande <oscar_obgg@hotmail.com>
2018-07-08 22:44:28 -0500

    fix: add redirect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import './App.css';
 
@@ -11,6 +11,7 @@ import store from "./store";
 import Gifs from "./components/Gifs/Gifs";
 import SearchResult from "./components/SearchResult/SearchResult";
 import Favorites from './components/Favorites/Favorites';
+import NotFound from './components/NotFound/NotFound'
 
 class App extends Component {
   render() {
@@ -21,6 +22,8 @@ class App extends Component {
             <Route exact path="/" component={Gifs} />
             <Route exact path="/search/:q" component={SearchResult} />
             <Route path="/favorites" component={Favorites} />
+            <Route path="*" component={NotFound} />
+            <Redirect to="not-found"/>
           </Switch>
         </Router>
       </Provider>
