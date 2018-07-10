3b75ed7279f973ab10d71965329c3da4f5d6aed4
Moises <moises.pedraza.contractor@bbva.com>
2018-07-09 21:28:08 -0500

    Refactor  Pass routes to the app as props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import registerServiceWorker from './registerServiceWorker';
+import routes from './routes'
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App routes={routes} />, document.getElementById('root'));
 registerServiceWorker();
