e88713e80a8df494c1a2a9d3a0edf9b31b637a55
Daniel Garcia <pel_daniel@hotmail.com>
2018-07-09 20:58:23 -0500

    GifGrid css with styled-components

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,22 +1,20 @@
 import React from 'react';
 
 import GifCard from './GifCard';
-
-import '../styles/GifGrid.css';
-
+import { GifGridWrapper } from '../styles/GifGrid.style.js';
 
 const GifGrid = (props) => {
   const { gifs } = props;
 
   return (
-    <div className="GifGrid mt">
+    <GifGridWrapper>
       { gifs.map((gif) =>
         <GifCard key={gif.id} {...gif} />
       )}
       { gifs.length === 0 &&
         <p>Search for something</p>
       }
-    </div>
+    </GifGridWrapper>
   );
 };
 
