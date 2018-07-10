a7b1d7a8444fb76ede30bde1f88b26fb491fe861
Moises <moises.pedraza.contractor@bbva.com>
2018-07-09 21:27:42 -0500

    Feat  Add routes of the app

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        route: '/',
+        name: 'home',
+        exact: true,
+    },
+    {
+        route: '/trending',
+        name: 'trending',
+        exact: false
+    },
+    {
+        route: '/favorites',
+        name: 'favorites',
+        exact: false
+    }
+]
