3ff181837e18a19bd0dbd8c98286f6d14699f2cd
Daniel Garcia <pel_daniel@hotmail.com>
2018-07-09 23:40:18 -0500

    Add index.css styles to Global styles file

--- src/styles/Global.style.js
+++ src/styles/Global.style.js
@@ -1,6 +1,12 @@
 import { injectGlobal } from 'styled-components';
 
 injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+
   .AppContainer {
     margin: 0 auto;
     width: 800px;
