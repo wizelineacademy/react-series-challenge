bc73b6d58f645be7552d67f679010d7b420d9b19
Daniel Garcia <pel_daniel@hotmail.com>
2018-07-09 22:44:42 -0500

    Use innerRef in SearchBar because of handling of 'refs' of styled-components

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux';
 
 import { SearchBarInput } from '../styles/SearchBar.style.js';
 
 import gifActions from '../actions/gifActions';
 
-const SearchBar = ({ dispatch }) => {
-  let input;
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <div>
-      <SearchBarInput
-        type="text"
-        placeholder="Search gifs!!"
-        ref={ node => input = node }
-        onKeyPress={ event => {
-          if(event.key === 'Enter' && input.value.trim()) {
-            dispatch(gifActions.creators.searchGifs(input.value));
-          }
-        }}
-      />
-    </div>
-  )
+  render() {
+    const { searchGifs } = this.props;
+    const self = this;
+
+    return (
+      <div>
+        <SearchBarInput
+          type="text"
+          placeholder="Search gifs!!"
+          innerRef={ node => { this.input = node } }
+          onKeyPress={ event => {
+            if(event.key === 'Enter' && self.input.value.trim()) {
+              searchGifs(self.input.value);
+            }
+          }}
+        />
+      </div>
+    )
+  }
 }
 
-export default connect()(SearchBar);
+const mapDispatchToProps = (dispatch) => {
+  const { searchGifs } = gifActions.creators;
+
+  return bindActionCreators({ searchGifs }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
