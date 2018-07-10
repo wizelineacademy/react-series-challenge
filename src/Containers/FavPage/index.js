004dd63a25e34a5cf4943f84e5360f1619a21a83
fragmnt02 <fragmnt02@gmail.com>
2018-07-10 17:31:07 -0500

    remove state from fav page

--- src/Containers/FavPage/index.js
+++ src/Containers/FavPage/index.js
@@ -4,19 +4,12 @@ import { connect } from 'react-redux'
 import Gif from "../../components/Gif"
 
 class FavPage extends Component {
-
-  getGifs(gifs){
-      const images=gifs && gifs.map((gif, index) => {return <Gif url={gif.url} id={gif.id} key={gif.id} />});
-      return images;
-  }
-
-
   render() {
     
     return (
         <React.Fragment>
         {
-          this.getGifs(this.props.gifs)
+          this.props.gifs && this.props.gifs.map((gif, index) => {return <Gif url={gif.url} id={gif.id} key={gif.id} />})
         }
         </React.Fragment>
     )
