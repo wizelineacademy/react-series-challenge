82b1a53ae6f6704ce9acdf24d4ca2c4bf1936258
Moises <moises.pedraza.contractor@bbva.com>
2018-07-11 11:18:37 -0500

    Refactor/Remove   Refactor all types and creators and remove unnecesary location

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,16 +1,20 @@
 const SEARCH_GET = 'SEARCH_GET'
 const SEARCH_FETCHED = 'SEARCH_FETCHED'
+const SEARCH_FAILED = 'SEARCH_FAILED'
 
-const fetchedSearch = (payload) => ({ type: SEARCH_FETCHED, payload })
-const getSearch = (payload) => ({ type: SEARCH_GET, payload })
+const getSearch = payload => ({ type: SEARCH_GET, payload })
+const fetchedSearch = payload => ({ type: SEARCH_FETCHED, payload })
+const failedSearch = payload => ({ type: SEARCH_FAILED, payload })
 
 export default {
-    creators: {
-        fetchedSearch,
-        getSearch
-    },
-    types: {
-        SEARCH_FETCHED,
-        SEARCH_GET
-    }
+  types: {
+    SEARCH_GET,
+    SEARCH_FETCHED,
+    SEARCH_FAILED
+  },
+  creators: {
+    getSearch,
+    fetchedSearch,
+    failedSearch
+  }
 }
--- src/actions/trending.js
+++ src/actions/trending.js
@@ -1,16 +1,20 @@
 const TRENDING_GET = 'TRENDING_GET'
 const TRENDING_FETCHED = 'TRENDING_FETCHED'
+const TRENDING_FAILED = 'TRENDING_FAILED'
 
-const getTrending = (payload) => ({ type: TRENDING_GET, payload })
-const fetchedTrending = (payload) => ({ type: TRENDING_FETCHED, payload })
+const getTrending = payload => ({ type: TRENDING_GET, payload })
+const fetchedTrending = payload => ({ type: TRENDING_FETCHED, payload })
+const failedTrending = payload => ({ type: TRENDING_FAILED, payload })
 
 export default {
     types: {
         TRENDING_GET,
-        TRENDING_FETCHED
+        TRENDING_FETCHED,
+        TRENDING_FAILED
     },
     creators: {
         getTrending,
-        fetchedTrending
+        fetchedTrending,
+        failedTrending
     }
 }
