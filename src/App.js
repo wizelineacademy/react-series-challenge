d0db0b1a4953900da8328e65815f5f305ff6df2a
AsterDecember <aster.december@gmail.com>
2018-07-11 10:48:27 -0500

    deleting css files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css'
 import FavElements from './components/FavElements';
 import ElementsView from './components/ElementsView';
 import {Route, Link, Redirect, Switch} from "react-router-dom";
@@ -11,10 +10,9 @@ export const ALL_GIFS_VIEW = '/allElements';
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
+      <div>
+        <header>
+          <h1>Giphys app</h1>
         </header>
           <div>
               <ul>
