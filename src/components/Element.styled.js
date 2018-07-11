c6ddf218103ac1f28d0a19d622c15d31dd2949d1
AsterDecember <aster.december@gmail.com>
2018-07-11 10:29:13 -0500

    Like icon now shows on hover

--- src/components/Element.styled.js
+++ src/components/Element.styled.js
@@ -5,26 +5,6 @@ const RelativeDiv = styled.div`
     position: relative;
 `;
 
-const IconFavBlue = styled.i`
-    color: blue;
-    position: absolute;
-    bottom: 1rem;
-    right: 1rem;
-`;
-const IconFav = styled.i`
-    position: absolute;
-    bottom: 1rem;
-    right: 1rem;
-`;
-
-const ElementImg = styled.img`
-    display: block;
-    border: 0;
-    width: 100%;
-    height: 150px;
-    max-height: 150px;
-`;
-
 const Shadow = styled.div`
     position: absolute;
     top: 0;
@@ -39,6 +19,37 @@ const Shadow = styled.div`
         background-color: rgba(0,0,0,0.7);
     }
 `;
+
+const IconFavBlue = styled.i`
+    color: blue;
+    position: absolute;
+    bottom: 1rem;
+    right: 1rem;
+    ${Shadow}:hover & {
+        visibility: visible;
+        opacity: 1;
+    }
+`;
+const IconFav = styled.i`
+    position: absolute;
+    bottom: 1rem;
+    right: 1rem;
+    visibility: hidden;
+    
+    ${Shadow}:hover & {
+        visibility: visible;
+        opacity: 1;
+    }
+`;
+
+const ElementImg = styled.img`
+    display: block;
+    border: 0;
+    width: 100%;
+    height: 150px;
+    max-height: 150px;
+`;
+
 
 
 export {
