16649f95c21c9af30d93468e0b7fee4012e58853
diego-d5000 <diego-d@outlook.com>
2018-07-11 22:02:05 -0500

    remove css index styles, added inject global styles (styled-components)

--- src/global.styled.js
+++ src/global.styled.js
@@ -1,6 +1,10 @@
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
 body {
   margin: 0;
   padding: 0;
   font-family: sans-serif;
   background-color: #fafafa;
 }
+`;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './global.styled';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
