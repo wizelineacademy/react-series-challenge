36e0dc8b6333960fb78f2c6ea0dccc97334f4394
Andre Cortes <andre.crts@gmail.com>
2018-07-12 00:31:19 -0500

    [add] test actions on searchbar

--- src/test/components/Searchbar.test.js
+++ src/test/components/Searchbar.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Searchbar from '../../components/Searchbar'
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import toJson from 'enzyme-to-json'
 import 'jest-styled-components'
 
@@ -12,4 +12,17 @@ test('Searchbar snapshot', () => {
   expect(toJson(component)).toMatchSnapshot();
 })
 
+describe('Search actions', () => {
+  it('handleKeyPress', () => {
+    const onHandleKeyPress = jest.fn();
+    const component = mount(
+      <Searchbar
+        handleKeyPress={onHandleKeyPress}
+      />
+    )
+    const input = component.find('input')
+    input.simulate('keyPress', {key: 's'});
+    expect(onHandleKeyPress).toHaveBeenCalled()
+  })
+})
 
