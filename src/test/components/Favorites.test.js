dccb6b3e9c8e62dd518a0eb9f8e7ebdc5ab96c76
Andre Cortes <andre.crts@gmail.com>
2018-07-12 00:44:13 -0500

    [add] test for toggleFavorite

--- src/test/components/Favorites.test.js
+++ src/test/components/Favorites.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Favorites from '../../components/Favorites'
-import { mount } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import toJson from 'enzyme-to-json'
 import configureStore from 'redux-mock-store'
 import 'jest-styled-components'
@@ -20,5 +20,23 @@ test('Home snapshot', () => {
     <Favorites store={store}/>,
   );
   expect(toJson(component)).toMatchSnapshot();
+})
+
+describe('Search actions', () => {
+  it('handleKeyPress', () => {
+    const initialState = {
+      content:{
+        data: {data: {tite:'title1', images:{original:{webp:'srcImage'}}}},
+        favorites:{}
+      }
+    }
+    const onHandleKeyPress = jest.fn();
+  const store = mockStore(initialState)
+    const component = shallow(
+      <Favorites store={store}/>,
+    )
+
+    component.dive().instance().toogleFavorite({}, true)
+  })
 })
 
