4de2a5d8847416c6ab72474b9a13176cd5432154
AsterDecember <aster.december@gmail.com>
2018-07-12 00:31:40 -0500

    config LocalStorage for testing components

--- src/test/components/FavElements.test.js
+++ src/test/components/FavElements.test.js
@@ -3,7 +3,9 @@ import { Provider } from 'react-redux';
 import FavoriteGifs from '../../components/FavElements';
 import renderer from 'react-test-renderer';
 import store from '../../store/index';
+import localStorageMock from './localStorageMock';
 
+//
 describe('FavElements', () => {
 
     it('should render without problems', () => {
