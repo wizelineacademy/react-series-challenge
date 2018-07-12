a1a69f8947271387307fe7d264fe5a5d9fbf1209
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-07-11 21:31:18 -0500

    ItemsList Component tested too

--- src/components/ItemsList/index.js
+++ src/components/ItemsList/index.js
@@ -22,3 +22,4 @@ const mapStateToProps = ({ itemsList }) => {
 };
 
 export default connect(mapStateToProps)(ItemsList);
+export { ItemsList };
--- src/components/ItemsList/index.test.js
+++ src/components/ItemsList/index.test.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import ItemsList from './index';
+import ItemsList, { ItemsList as ItemsListComponent } from './index';
 import store from '../../store';
+
+const onClickMock = jest.fn();
+
+const testCurrentItemsList = [
+  {
+    title: 'happy emma watson GIF',
+    id: 'WsKVAem02Efuw',
+    images: {
+      original: {
+        url: 'https://media1.giphy.com/media/WsKVAem02Efuw/giphy.gif',
+      },
+    },
+    favorite: false,
+  },
+  {
+    title: 'emma watson GIF',
+    id: 'rSsoG2WPmzOIE',
+    images: {
+      original: {
+        url: 'https://media3.giphy.com/media/rSsoG2WPmzOIE/giphy.gif',
+      },
+    },
+    favorite: false,
+  },
+];
 
 describe('test ItemsList Render', () => {
   it('should render component structure', () => {
@@ -12,5 +37,14 @@ describe('test ItemsList Render', () => {
     const rendered = shallow(<ItemsList store={store} />);
     const { currentItemsList } = rendered.props();
     expect(currentItemsList.length).toBe(0);
+  });
+  it('should render ItemsList Component', () => {
+    const renderedComponent = shallow(
+      <ItemsListComponent
+        currentItemsList={testCurrentItemsList}
+        favoritesButton={onClickMock}
+      />
+    );
+    expect(renderedComponent).toMatchSnapshot();
   });
 });
