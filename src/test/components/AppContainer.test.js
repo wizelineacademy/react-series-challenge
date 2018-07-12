62cbd74106e332b25f96ad575a8d161afccdcca4
Daniel Garcia <pel_daniel@hotmail.com>
2018-07-11 22:14:17 -0500

    Test AppContainer connected component

--- src/test/components/AppContainer.test.js
+++ src/test/components/AppContainer.test.js
@@ -1,13 +1,23 @@
 import React from 'react';
+
 import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
 
-import { AppContainer } from '../../components/AppContainer';
+import ConnectedAppContainer, { AppContainer } from '../../components/AppContainer';
 
-describe('Header component', () => {
+
+describe('AppContainer component', () => {
   test('should shallow correctly', () => {
     const getTrendingGifs = () => {};
     const header = shallow(<AppContainer getTrendingGifs={ getTrendingGifs } />);
 
     expect(header).toMatchSnapshot();
+  });
+
+  test('should shallow connected component correctly', () => {
+    const store = configureStore()({ favorites: [{ id: 3 }], gifs: [] });
+    const container = shallow(<ConnectedAppContainer store={ store } />);
+
+    expect(container).toMatchSnapshot();
   });
 });
