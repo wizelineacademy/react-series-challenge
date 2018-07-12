45a775df3b8c3fc9844e7292108c4d77aeb3a64a
Fernando Gomez <gomezhyuuga@gmail.com>
2018-07-12 03:26:19 -0500

    Improved Search test

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Dispatch, bindActionCreators } from 'redux';
+import { Dispatch, bindActionCreators, Store, ActionCreator } from 'redux';
 
 import { search_trending, fetch_gifs as fetch_gifs_action } from '../actions/gifs';
 
 import { SearchInput } from './Search.style';
 
-class Search extends React.Component<any> {
+export class Search extends React.Component<any> {
     public onSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
         const term = event.target.value;
         const { search, fetch_gifs } = this.props;
 
         // Search bar is cleared
-        if (term.length === 0 || !term) fetch_gifs();
+        if (term.length === 0 || !term) return fetch_gifs();
 
         // Only search with at least 3 chars
         if (term.length >= 3) return search(term);
@@ -34,4 +34,5 @@ function mapDispatchToProps(dispatch: Dispatch) {
         search: search_trending,
     }, dispatch);
 }
+
 export default connect(mapStateToProps, mapDispatchToProps)(Search);
