98211aca79231ab4aca26fa05326adf9ec871074
fragmnt02 <fragmnt02@gmail.com>
2018-07-12 00:51:24 -0500

    unused state

--- src/Containers/HomePage/index.js
+++ src/Containers/HomePage/index.js
@@ -4,10 +4,6 @@ import { connect } from 'react-redux'
 import Gif from "../../components/Gif"
 
 class HomePage extends Component {
-  state = {
-    openFullScreen: false,
-    selectedImageUrl: ''
-  }
 
   componentDidMount() {
     const { queryTrending } = this.props;
