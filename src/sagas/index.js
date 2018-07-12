c2213bd99148ddbe8bc1c798556339eed40a29b8
Gabriel Garcia <gabrielgsdev@gmail.com>
2018-07-11 21:37:15 -0500

    Fetch data from saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,5 +1,6 @@
-import { put, takeEvery, all } from 'redux-saga/effects'
+import { put, takeEvery, all, call } from 'redux-saga/effects'
 import * as actionTypes from '../actions'
+import axios from 'axios'
 
 function* getFavoriteGifs(action) {
   try {
@@ -34,13 +35,42 @@ function* watchFavoriteGifs() {
   yield takeEvery( actionTypes.FAVORITE_GIFS_INIT, getFavoriteGifs )
 }
 
+function* fetchData(url) {
+
+  return yield axios.get(url);
+}
+
+function* fetchingData(action) {
+  const BASE_URL = process.env.REACT_APP_BASE_URL
+  const API_KEY = process.env.REACT_APP_API_KEY
+  let { query } = action
+
+  const endpoint = !!query ? 'search' : 'trending'
+
+  query = !!query ? `&q=${query}` : ''
+  const url = `${BASE_URL}${endpoint}?api_key=${API_KEY}&limit=25${query}`;
+
+  try {
+    const data = yield call(fetchData, url)
+    yield put(actionTypes.fetchDataSuccess(data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function* watchFetchData() {
+  yield takeEvery(actionTypes.FETCH_DATA_REQUEST, fetchingData)
+}
+
 function* watchToggleFavorite(){
   yield takeEvery(actionTypes.FAVORITE_TOGGLE_START, toogleFavorite)
 }
 
+
 export default function* rootSaga() {
   yield all([
     watchFavoriteGifs(),
-    watchToggleFavorite()
+    watchToggleFavorite(),
+    watchFetchData()
   ])
 }
