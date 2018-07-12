650344a925e2162a3161488bcc49718d9571a5c9
Alain Perez Chevanier <alain.chevanier@wizeline.com>
2018-07-12 17:18:11 -0500

    TESTS ADDED FOR COMPONENT NAVBAR

---
+++ src/components/NavBar/NavBar.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
+import NavBar from './NarBar';
+
+jest.mock('react-router-dom', () => ({ Link: ({ children, ...rest }) => (<a href="#a" {...rest}>{children}</a>) }));
+
+describe('NavBar component', () => {
+  let links = null;
+
+  beforeEach(() => {
+    links = [
+      { url: '/a', isActive: false, text: 'a' },
+      { url: '/b', isActive: true, text: 'b' },
+    ];
+  });
+
+  it('render as teh snapshot says', () => {
+    const tree = renderer.create((
+      <NavBar links={links} type="text" />
+    )).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
