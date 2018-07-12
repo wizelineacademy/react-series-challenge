195f0fc3a86a08f1debc9702cca7cee8313f1140
Yoel Monzon <yoelfme@hotmail.com>
2018-07-11 21:45:24 -0500

    [add] search component for favorites and home

---
+++ src/components/search/Search.style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Search = styled.input`
+  width: 30%;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  background-color: white;
+  background-image: url('./icons/search.png');
+  background-position: 10px 10px;
+  background-repeat: no-repeat;
+  padding: 12px 20px 12px 40px;
+  -webkit-transition: width 0.4s ease-in-out;
+  transition: width 0.4s ease-in-out;
+
+  &:focus {
+    width: 80%;
+  }
+`;
