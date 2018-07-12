882b7ec030bf9c0141d24a064aeed719171087b7
Alfonso Ruiz <alfonso.ruiz.da@gmail.com>
2018-07-12 00:13:49 -0500

    local reducer test

--- src/redux/reducers/__test__/local.test.js
+++ src/redux/reducers/__test__/local.test.js
@@ -3,12 +3,11 @@ import reducer, { initialState } from "./../local";
 
 describe("local reducer >", () => {
 
-  it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual(
-      {ids: [], likes: []}
-    )
-  });
+  const emptyState = { ids: [], likes: [] };
 
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(emptyState)
+  });
 
   it('should handle LOCALSTORAGE_DATA', () => {
     // This func overwrite previous one
@@ -20,15 +19,38 @@ describe("local reducer >", () => {
     })).toEqual(data)
   });
 
-  // it('should handle LOCALSTORAGE_DATA', () => {
-  //   // This func overwrite previous one
-  //   const data = { likes: [{ example: 1 }], ids: ['randomNum'] };
-  //   expect(reducer({}, {
-  //     type: actions.LOCALSTORAGE_DATA, payload: {
-  //       data
-  //     }
-  //   })).toEqual(data)
-  // });
+  it('should handle LOCALSTORAGE_MODIFY when add', () => {
+    const object = { id: 'myRandomId', randomData: { a: 1, b: 2 } };
+    const result = { likes: [object], ids: [object.id] };
+    // This func overwrite previous one
+    expect(reducer(emptyState, {
+      type: actions.LOCALSTORAGE_MODIFY, payload: {
+        object
+      }
+    })).toEqual(result)
+  });
+
+  it('should handle LOCALSTORAGE_MODIFY when remove', () => {
+    const object = { id: 'myRandomId', randomData: { a: 1, b: 2 } };
+    const result = { likes: [object], ids: [object.id] };
+
+    // This func overwrite previous one
+    expect(reducer(result, {
+      type: actions.LOCALSTORAGE_MODIFY, payload: {
+        object
+      }
+    })).toEqual(initialState)
+  });
+
+  it('should handle LOCALSTORAGE_FIND', () => {
+    const filter = 'myFilter'
+    // This func overwrite previous one
+    expect(reducer(initialState, {
+      type: actions.LOCALSTORAGE_FIND, payload: {
+        filter
+      }
+    })).toEqual({ ...initialState, filter })
+  });
 
 
 });
