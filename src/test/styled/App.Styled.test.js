621452e37169d1b98207d5659129cf2644bd1129
ErickRc <erick@spaceshiplabs.com>
2018-07-11 22:04:01 -0500

    uppercase fix

--- src/test/styled/App.Styled.test.js
+++ src/test/styled/App.Styled.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'enzyme';
-import { AppStyled } from '../../styled/AppStyled.Styled';
+import { AppStyled } from '../../styled/AppStyled.styled';
 
 describe('App style', () => {
 	it('App style should work', () => {
