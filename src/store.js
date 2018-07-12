c1bcb8f1933d2b0f907e69b202f8396208841e5a
diego-d5000 <diego-d@outlook.com>
2018-07-12 00:19:21 -0500

    add enhancer to persist favorites

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
-import { applyMiddleware, compose, createStore } from 'redux'
-import createSagaMiddleware from 'redux-saga'
+import { applyMiddleware, compose, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-import monitorReducersEnhancer from './enhancers/monitorReducers'
-import loggerMiddleware from './middleware/logger'
-import rootReducer from './reducers'
+import monitorReducersEnhancer from './enhancers/monitorReducers';
+import loggerMiddleware from './middleware/logger';
+import rootReducer from './reducers';
 import rootSaga from './sagas';
+import persistState from 'redux-localstorage';
+
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -12,7 +14,7 @@ export default function configureStore(preloadedState) {
   const middlewares = [loggerMiddleware, sagaMiddleware]
   const middlewareEnhancer = applyMiddleware(...middlewares)
 
-  const enhancers = [middlewareEnhancer, monitorReducersEnhancer]
+  const enhancers = [middlewareEnhancer, monitorReducersEnhancer, persistState('favorites')]
   const composedEnhancers = compose(...enhancers)
 
   const store = createStore(rootReducer, preloadedState, composedEnhancers)
